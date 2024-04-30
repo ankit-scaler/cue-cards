a1fb10512fc594cd7af0d7aad9894da454381eed
ankit-scaler <114554881+ankit-scaler@users.noreply.github.com>
2024-04-30 13:59:25 +0530

    Update RishiPrakash-Alumni.rb

--- RishiPrakash-Alumni.rb
+++ RishiPrakash-Alumni.rb
@@ -10,12 +10,23 @@ whatsapp_number: 9992331690
 pre_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/072/194/original/accenture.png?1714069892
 post_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/072/196/original/microsoft.png?1714069933
 hours_attended: 
-problems_solved: 
+problems_solved: 305
 mentor_details: [
-
+{
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/071/877/original/Shivank_2.png?1713936614',
+    name: 'Prateek Narang',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/071/878/original/google_1.svg?1713936640',
+    topic: 'Taught Prakash DSA'
+  },
+  {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/071/879/original/Naman_Bhalla_02_5.png?1713936684',
+    name: 'Naman Bhalla',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/071/878/original/google_1.svg?1713936640',
+    topic: 'Taught Prakash HLD/LLD'
+  }
 ]
 additional_skills_count: 26+
-salary_hike: 
+salary_hike: 112%
 ---
 
 # Skills
