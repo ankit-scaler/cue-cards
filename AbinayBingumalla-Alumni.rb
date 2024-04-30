d6d87eab4e9176b9150227472bb97fc6c255d1b2
Saurabh Kumar Singh <sksusha@Saurabhs-MacBook-Air.local>
2024-04-30 14:49:06 +0530

    Updated the details of Rishi and Abinay

--- AbinayBingumalla-Alumni.rb
+++ AbinayBingumalla-Alumni.rb
@@ -1,17 +1,29 @@
 ---
 alumni_name: Abinay Bingumalla
-alumni_image_url: 
+alumni_image_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/016/284/original/success-story-image_id.png?1665563964
 role: Full Stack
-batch: Jan20 Scaler Batch
+batch: January 20 EliteX
 location: Bangalore
 card_type: alumni_card
 linkedin_url: https://www.linkedin.com/in/abinayb/
 whatsapp_number: 9703535151
 pre_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/072/197/original/relianceJio.png?1714070781
 post_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/072/196/original/microsoft.png?1714069933
-hours_attended: 
-problems_solved: 
+hours_attended: 104.46
+problems_solved: 350
 mentor_details: [
+    {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/071/877/original/Shivank_2.png?1713936614',
+    name: 'Prateek Narang',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/071/878/original/google_1.svg?1713936640',
+    topic: 'Taught Abinay DSA'
+  },
+  {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/071/879/original/Naman_Bhalla_02_5.png?1713936684',
+    name: 'Naman Bhalla',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/071/878/original/google_1.svg?1713936640',
+    topic: 'Taught Abinay HLD/LLD'
+  }
  
 ]
 additional_skills_count: 19+
--- RishiPrakash-Alumni.rb
+++ RishiPrakash-Alumni.rb
@@ -1,21 +1,32 @@
 ---
 alumni_name: Rishi Prakash
-alumni_image_url: 
+alumni_image_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/016/272/original/success-story-image_id.png?1665563945
 role: Software Engineer 2
-batch: Jan20 Scaler Batch
+batch: January 20 EliteX
 location: Gurgaon
 card_type: alumni_card
 linkedin_url: https://www.linkedin.com/in/rishi-prakash-developer/
 whatsapp_number: 9992331690
 pre_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/072/194/original/accenture.png?1714069892
 post_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/072/196/original/microsoft.png?1714069933
-hours_attended: 
-problems_solved: 
+hours_attended: 63.15
+problems_solved: 305
 mentor_details: [
-
+    {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/071/877/original/Shivank_2.png?1713936614',
+        name: 'Prateek Narang',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/071/878/original/google_1.svg?1713936640',
+        topic: 'Taught Prakash DSA'
+      },
+      {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/071/879/original/Naman_Bhalla_02_5.png?1713936684',
+        name: 'Naman Bhalla',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/071/878/original/google_1.svg?1713936640',
+        topic: 'Taught Prakash HLD/LLD'
+      }
 ]
 additional_skills_count: 26+
-salary_hike: 
+salary_hike: 200
 ---
 
 # Skills
