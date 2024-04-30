2f9d9544c2268f65d7b3a96985ef7a0564b1e064
ankit-scaler <114554881+ankit-scaler@users.noreply.github.com>
2024-04-30 14:01:34 +0530

    Update AbinayBingumalla-Alumni.rb

--- AbinayBingumalla-Alumni.rb
+++ AbinayBingumalla-Alumni.rb
@@ -12,7 +12,18 @@ post_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/as
 hours_attended: 
 problems_solved: 
 mentor_details: [
- 
+ {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/071/877/original/Shivank_2.png?1713936614',
+    name: 'Prateek Narang',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/071/878/original/google_1.svg?1713936640',
+    topic: 'Taught Prakash DSA'
+  },
+  {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/071/879/original/Naman_Bhalla_02_5.png?1713936684',
+    name: 'Naman Bhalla',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/071/878/original/google_1.svg?1713936640',
+    topic: 'Taught Prakash HLD/LLD'
+  }
 ]
 additional_skills_count: 19+
 salary_hike: 112%
