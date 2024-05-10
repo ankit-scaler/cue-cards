5a9c27f88b413b91076e4df135eb51e4b7a0fea6
ankit-scaler <114554881+ankit-scaler@users.noreply.github.com>
2024-05-10 13:34:25 +0530

    Update TanmayKacker-Instructor.rb

--- TanmayKacker-Instructor.rb
+++ TanmayKacker-Instructor.rb
@@ -5,17 +5,25 @@ class_rating: 4.71
 location: 
 card_type: instructor_card
 linkedin_url: 
-introduction_bio: 
-num_learners: 
-hour_classes: 
+introduction_bio: "Ex-Tech Lead, Locus
+Ex-Senior Software Developer, Practo
+Postgraduate student, Cranfield University, UK"
+num_learners: 4041
+hour_classes: 378
 work_history: [
     {
-        "company": "",
-        "title": '',
-        "company_logo_url": ""
+        "company": "Locus",
+        "title": 'Tech Lead',
+        "company_logo_url": "https://locus.sh/assets/img/logo/locus-white.svg"
+    },
+    {
+        "company": "Practo",
+        "title": 'Senior Software Developer',
+        "company_logo_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSV9toIconMh1zl2vB7_5QYzgXO7pu2iTz2IzI4Jtdu&s"
     },
 ]
 students_working_at_company_logos: [
+    
 ]
 ---
 
