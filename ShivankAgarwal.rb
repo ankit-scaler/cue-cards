6fe1cb2f68c26cca375d05740a92d7643c7a60a9
ankit-scaler <114554881+ankit-scaler@users.noreply.github.com>
2024-05-10 20:43:58 +0530

    Update ShivankAgarwal.rb

--- ShivankAgarwal.rb
+++ ShivankAgarwal.rb
@@ -1,1 +1,39 @@
 
+---
+instructor_name: Shivank Agrawal
+instructor_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/545/original/Screenshot_2024-05-10_at_20.26.56.png?1715353365'
+class_rating: 4.8
+location: Bengaluru, KA, India
+card_type: instructor_card
+linkedin_url: https://www.linkedin.com/in/shivankagrawal/
+introduction_bio: "Academy: Ex - Senior Software Manager in Microsoft
+DSML: Senior V.P., Data Science at Scaler"
+num_learners: 7073
+hour_classes: 1629
+work_history: [
+    {
+        "company": "Microsoft",
+        "title": 'Senior Software Development Manager',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/548/original/kisspng-microsoft-logo-organization-san-francisco-cloud-co-partner-5ae00ee7dc7b67.5436261215246333199031.png?1715353831"
+    },
+    {
+        "company": "InMob",
+        "title": 'Engineering Manager',
+        "company_logo_url": "https://media.licdn.com/dms/image/C560BAQFkgPvhb-kCbw/company-logo_200_200/0/1650697007483/inmobi_logo?e=1723680000&v=beta&t=wI1dB_PF0b3iUZ8I_3ZfVFYc9V8X00X4F2mT_m8c-Tc"
+    },
+    {
+        "company": "Oracle",
+        "title": 'oftware Development Manager',
+        "company_logo_url": "https://media.licdn.com/dms/image/D4E0BAQHYCgYovUuPtQ/company-logo_200_200/0/1665755678957/oracle_logo?e=1723680000&v=beta&t=Qf2ehQG7LDkPh9SR7Wa8257Xe_WXsOYOyiQ9cDHO7Uw"
+    },
+  
+]
+students_working_at_company_logos: [
+  
+]
+---
+
+# Skill Tags
+- System Design Expert
+- DSA Expert
+- Java Expert
