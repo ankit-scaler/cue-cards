8537af9364e0cf99471001855f0cea4c34b74bba
sksusha8853 <sksusha8853@gmail.com>
2024-05-14 19:53:42 +0530

    completed Shriram Bhat profile

--- AyushMishra-Alumni.rb
+++ AyushMishra-Alumni.rb
@@ -1,35 +1,38 @@
 ---
 alumni_name: Ayush Mishra
-alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/016/613/original/success-story-image_id.png?1665564339'
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/864/original/AyushMishra.webp?1715694848'
 role: SDE 2
 batch: Software Development April 2020
 location: Kanpur
 card_type: alumni_card
 linkedin_url: 'https://www.linkedin.com/in/ayush-mishra5/'
 whatsapp_number: 
-pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/180/original/samsung.jpeg?1715119857'
-post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/072/196/original/microsoft.png?1714069933'
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/865/original/samsung.webp?1715694954'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/866/original/microsoft_%281%29.webp?1715695045'
 hours_attended: 
 problems_solved: 
 mentor_details: [
-  {
-    image: '',
-    name: '',
-    company_logo_url: '',
-    topic: ''
-  },
-  {
-    image: '',
-    name: '',
-    company_logo_url: '',
-    topic: ''
-  },
+    {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/859/original/namanBhalla_%281%29.webp?1715691506',
+        name: 'Naman Bhalla',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585',
+        topic: 'Taught LLD'
+      },
+      {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/861/original/anshuman_%281%29.webp?1715691886',
+        name: 'Anshuman Singh',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/862/original/scaler_%281%29.webp?1715692250',
+        topic: 'Taught HLD'
+      },
   
 ]
-additional_skills_count: 
-salary_hike: 424
+additional_skills_count: 27+
+salary_hike: 424%
 ---
 
 # Skills
+- Algorithms
+- C++
+- C#
 - Python
-- SQL
+- System Design
--- ShriramBhat-Alumni.rb
+++ ShriramBhat-Alumni.rb
@@ -1,42 +1,38 @@
 ---
 alumni_name: Shriram Bhat
-alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/016/302/original/success-story-image_id.png?1665563991'
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/868/original/Shriram_Bhat.webp?1715696022'
 role: Software Engineer 3
 batch: Academy Jun22 Intermediate
 location: Bangalore
 card_type: alumni_card
 linkedin_url: 'https://www.linkedin.com/in/shriram-bhat-berolli/'
 whatsapp_number: 8884680706
-pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/181/original/hcl.jpeg?1715121020'
-post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/182/original/paypal.png?1715121457'
-hours_attended: 277.42
-problems_solved: 179
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/869/original/hcl.webp?1715696143'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/871/original/paypl.webp?1715696357'
+hours_attended: 278+
+problems_solved: 179+
 mentor_details: [
-  {
-    image: '',
-    name: 'Aditya Narain Gupta',
-    company_logo_url: '',
-    topic: 'Taught Shriram Backend (Java Spring/Springboot)'
-  },
-  {
-    image: '',
-    name: 'Majumder',
-    company_logo_url: '',
-    topic: 'Taught Shriram High Level Design'
-  },
-  {
-    image: '',
-    name: 'Rajmani Patel',
-    company_logo_url: '',
-    topic: 'Taught Shriram LLD'
-  },
+    {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/859/original/namanBhalla_%281%29.webp?1715691506',
+        name: 'Naman Bhalla',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585',
+        topic: 'Taught LLD'
+      },
+      {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/861/original/anshuman_%281%29.webp?1715691886',
+        name: 'Anshuman Singh',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/862/original/scaler_%281%29.webp?1715692250',
+        topic: 'Taught HLD'
+      },
   
   
 ]
 additional_skills_count: 40+
-salary_hike: 
 ---
 
 # Skills
 - Java
 - Spring Boot
+- Data Structures
+- Algorithms
+- Systems Design
