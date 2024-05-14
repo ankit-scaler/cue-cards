7986c955361d527a76bd536895edc7404f705aff
sksusha8853 <sksusha8853@gmail.com>
2024-05-14 19:14:11 +0530

    LLD/HLD

--- AbiramiS-Alumni.rb
+++ AbiramiS-Alumni.rb
@@ -17,13 +17,13 @@ mentor_details: [
     image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/859/original/namanBhalla_%281%29.webp?1715691506',
     name: 'Naman Bhalla',
     company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585',
-    topic: 'Taught HLD/LLD'
+    topic: 'Taught LLD'
   },
   {
     image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/861/original/anshuman_%281%29.webp?1715691886',
     name: 'Anshuman Singh',
     company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/862/original/scaler_%281%29.webp?1715692250',
-    topic: 'Taught DSA'
+    topic: 'Taught HLD'
   },
   
 ]
