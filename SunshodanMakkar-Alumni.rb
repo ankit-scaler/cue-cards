41057fc4f6771cadb3700e6b8e4c040180c96c0e
ankit-scaler <114554881+ankit-scaler@users.noreply.github.com>
2024-05-14 15:17:49 +0530

    Update SunshodanMakkar-Alumni.rb

--- SunshodanMakkar-Alumni.rb
+++ SunshodanMakkar-Alumni.rb
@@ -1,25 +1,36 @@
 ---
-alumni_name: Sunshodan Makkar
-alumni_image_url: 
+alumni_name: Sunshodan Makkar	
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/835/original/Sunshodan_Makkar.webp?1715679500'
 role: Data Analyst
-batch: Data Science & ML September 2022
-location: Jalandhar
+batch: DSML Dec22 Beginner Tue	
+location: Pune, MH, India
 card_type: alumni_card
-linkedin_url: https://www.linkedin.com/in/sunshodan
-whatsapp_number: 8284064001
-pre_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/072/190/original/theMathCompany.png?1714067598
-post_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/072/191/original/amber.jpeg?1714067617
-hours_attended: 
-problems_solved: 
+linkedin_url: https://www.linkedin.com/in/sunshodan/
+whatsapp_number: 
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/836/original/themathcompany_logo.webp?1715679794'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/837/original/amberstudent_logo.webp?1715679903'
+hours_attended: 214
+problems_solved: 620
 mentor_details: [
-
+  {
+    image: '',
+    name: '',
+    company_logo_url: '',
+    topic: ''
+  },
+  {
+    image: '',
+    name: '',
+    company_logo_url: '',
+    topic: ''
+  },
+  
 ]
-additional_skills_count: 
+additional_skills_count: 22+
 salary_hike: 
 ---
 
 # Skills
-- Python
+- Data Analytics
 - SQL
-- Data Analysis
-- Machine Learning
+- Tableau
