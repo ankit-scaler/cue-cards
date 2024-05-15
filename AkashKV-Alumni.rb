fd9fe7a49296aa28a13f8bb5071d28925e6c5c71
sksusha8853 <sksusha8853@gmail.com>
2024-05-15 22:46:23 +0530

    Akash KV whole profile completed

--- AkashKV-Alumni.rb
+++ AkashKV-Alumni.rb
@@ -1,22 +1,22 @@
 ---
-alumni_name: Rohit Kamra
-alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/074/041/original/rohit.webp?1715791399'
+alumni_name: Akash KV
+alumni_image_url: Data Science & ML June 2022
 role: Data Scientist
-batch: Data Science & ML March 2022
-location: Bengaluru
+batch: Data Science & ML June 2022
+location: Bangalore
 card_type: alumni_card
-linkedin_url: 'https://www.linkedin.com/in/rohitkamra/'
-whatsapp_number: 8952074692
-pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/074/042/original/accenture.webp?1715791882' 
-post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/074/043/original/brillio.webp?1715791988'
-hours_attended: 672
-problems_solved: 1548
+linkedin_url: 'https://www.linkedin.com/in/akash-k-v-10983215a/'
+whatsapp_number: 9739048264
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/074/045/original/unacademy.webp?1715792837'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/074/046/original/terrabluext.webp?1715793020'
+hours_attended: 489
+problems_solved: 1029
 mentor_details: [
     {
         image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/982/original/shivank_%281%29.webp?1715769654',
         name: 'Shivank Agarwal',
         company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/983/original/microsoft_%281%29_%282%29.webp?1715769840',
-        topic: 'Taught SQL'
+        topic: 'Taught Advance DSA'
       },
       {
         image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/985/original/surraj_%282%29.webp?1715770309',
@@ -26,11 +26,12 @@ mentor_details: [
       },
   
 ]
-additional_skills_count: 50
+additional_skills_count: 25
 ---
 
 # Skills
-- Data Science
-- Machine Learning
-- Java
+- Statistical Data Analysis
 - SQL
+- Deep Learning
+
+
