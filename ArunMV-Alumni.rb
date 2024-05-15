84f1b5ddcaf21155a6037c50ada0470ac19822c9
sksusha8853 <sksusha8853@gmail.com>
2024-05-15 16:30:48 +0530

    Corrected Arun and Sunshodan data

--- ArunMV-Alumni.rb
+++ ArunMV-Alumni.rb
@@ -9,24 +9,24 @@ linkedin_url: https://www.linkedin.com/in/arun-m-v-398952145/
 whatsapp_number: 
 pre_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/833/original/qualcomm_logo.webp?1715678608
 post_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/834/original/informatica_logo.jpeg?1715678838
-hours_attended: 816+
-problems_solved: 1479+
+hours_attended: 816
+problems_solved: 1479
 mentor_details: [
   {
-    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/859/original/namanBhalla_%281%29.webp?1715691506',
-    name: 'Naman Bhalla',
-    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585',
-    topic: 'Taught LLD'
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/982/original/shivank_%281%29.webp?1715769654',
+    name: 'Shivank Agarwal',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/983/original/microsoft_%281%29_%282%29.webp?1715769840',
+    topic: 'Taught Advance DSA'
   },
   {
-    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/861/original/anshuman_%281%29.webp?1715691886',
-    name: 'Anshuman Singh',
-    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/862/original/scaler_%281%29.webp?1715692250',
-    topic: 'Taught HLD'
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/985/original/surraj_%282%29.webp?1715770309',
+    name: 'Suraaj Hasija',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/986/original/mastercard_%281%29.webp?1715770428',
+    topic: 'Taught Case Study'
   },
   
 ]
-additional_skills_count: 20+
+additional_skills_count: 20
 salary_hike: 110%
 ---
 
--- SunshodanMakkar-Alumni.rb
+++ SunshodanMakkar-Alumni.rb
@@ -9,23 +9,23 @@ linkedin_url: https://www.linkedin.com/in/sunshodan/
 whatsapp_number: 
 pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/836/original/themathcompany_logo.webp?1715679794'
 post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/837/original/amberstudent_logo.webp?1715679903'
-hours_attended: 214+
-problems_solved: 620+
+hours_attended: 214
+problems_solved: 620
 mentor_details: [
   {
-    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/859/original/namanBhalla_%281%29.webp?1715691506',
-    name: 'Naman Bhalla',
-    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585',
-    topic: 'Taught LLD'
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/982/original/shivank_%281%29.webp?1715769654',
+    name: 'Shivank Agarwal',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/983/original/microsoft_%281%29_%282%29.webp?1715769840',
+    topic: 'Taught Advance Python'
   },
   {
-    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/861/original/anshuman_%281%29.webp?1715691886',
-    name: 'Anshuman Singh',
-    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/862/original/scaler_%281%29.webp?1715692250',
-    topic: 'Taught HLD'
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/985/original/surraj_%282%29.webp?1715770309',
+    name: 'Suraaj Hasija',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/986/original/mastercard_%281%29.webp?1715770428',
+    topic: 'Taught MLOps'
   },
 ]
-additional_skills_count: 22+
+additional_skills_count: 22
 ---
 
 # Skills
