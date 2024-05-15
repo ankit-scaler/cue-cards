71c2d0187b80824913c68a496f839abff4720265
sksusha8853 <sksusha8853@gmail.com>
2024-05-15 15:17:15 +0530

    Completed Profile for Shivank

--- ShivankAgarwal-Instructor.rb
+++ ShivankAgarwal-Instructor.rb
@@ -1,30 +1,31 @@
-
 ---
 instructor_name: Shivank Agrawal
-instructor_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/545/original/Screenshot_2024-05-10_at_20.26.56.png?1715353365'
+instructor_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/958/original/shivank.webp?1715765931'
 class_rating: 4.8
 location: Bengaluru, KA, India
 card_type: instructor_card
-linkedin_url: https://www.linkedin.com/in/shivankagrawal/
-introduction_bio: "Academy: Ex - Senior Software Manager in Microsoft
-DSML: Senior V.P., Data Science at Scaler"
+linkedin_url: 'https://www.linkedin.com/in/shivankagrawal/'
+introduction_bio: "Ex-Senior Software Development Manager at Microsoft.
+Ex-Engineering Manager at Inmob.
+Ex-Software Development Manager at Oracle
+"
 num_learners: 7073+
 hour_classes: 1629+
 work_history: [
     {
         "company": "Microsoft",
         "title": 'Senior Software Development Manager',
-        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/548/original/kisspng-microsoft-logo-organization-san-francisco-cloud-co-partner-5ae00ee7dc7b67.5436261215246333199031.png?1715353831"
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/961/original/microsoft_%281%29_%281%29.webp?1715766043"
     },
     {
-        "company": "InMob",
+        "company": "InMobi",
         "title": 'Engineering Manager',
-        "company_logo_url": "https://media.licdn.com/dms/image/C560BAQFkgPvhb-kCbw/company-logo_200_200/0/1650697007483/inmobi_logo?e=1723680000&v=beta&t=wI1dB_PF0b3iUZ8I_3ZfVFYc9V8X00X4F2mT_m8c-Tc"
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/963/original/inmobi.webp?1715766220"
     },
     {
         "company": "Oracle",
         "title": 'oftware Development Manager',
-        "company_logo_url": "https://media.licdn.com/dms/image/D4E0BAQHYCgYovUuPtQ/company-logo_200_200/0/1665755678957/oracle_logo?e=1723680000&v=beta&t=Qf2ehQG7LDkPh9SR7Wa8257Xe_WXsOYOyiQ9cDHO7Uw"
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/965/original/oracle.webp?1715766308"
     },
   
 ]
