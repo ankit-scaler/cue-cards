c16560a81c982e734cdc43a1e3fe15f9a1da0165
sksusha8853 <sksusha8853@gmail.com>
2024-05-15 22:29:59 +0530

    Rohit profile completed

---
+++ RohitKamra-Alumni.rb
@@ -0,0 +1,36 @@
+---
+alumni_name: Rohit Kamra
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/074/041/original/rohit.webp?1715791399'
+role: Data Scientist
+batch: Data Science & ML March 2022
+location: Bengaluru
+card_type: alumni_card
+linkedin_url: 'https://www.linkedin.com/in/rohitkamra/'
+whatsapp_number: 8952074692
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/074/042/original/accenture.webp?1715791882' 
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/074/043/original/brillio.webp?1715791988'
+hours_attended: 672
+problems_solved: 1548
+mentor_details: [
+    {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/982/original/shivank_%281%29.webp?1715769654',
+        name: 'Shivank Agarwal',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/983/original/microsoft_%281%29_%282%29.webp?1715769840',
+        topic: 'Taught Advance SQL'
+      },
+      {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/985/original/surraj_%282%29.webp?1715770309',
+        name: 'Suraaj Hasija',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/986/original/mastercard_%281%29.webp?1715770428',
+        topic: 'Taught Business Case'
+      },
+  
+]
+additional_skills_count: 50
+---
+
+# Skills
+- Data Science
+- Machine Learning
+- Java
+- SQL
