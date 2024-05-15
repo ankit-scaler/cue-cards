8a81d66be5af7c27a4b7ceb22850235c34d99567
sksusha8853 <sksusha8853@gmail.com>
2024-05-15 15:54:06 +0530

    Ayush Mishra Profile completed

--- AyushMishra-Alumni.rb
+++ AyushMishra-Alumni.rb
@@ -2,22 +2,22 @@
 alumni_name: Ayush Mishra
 alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/864/original/AyushMishra.webp?1715694848'
 role: SDE 2
-batch: Software Development April 2020
+batch: April 20 SuperX
 location: Kanpur
 card_type: alumni_card
 linkedin_url: 'https://www.linkedin.com/in/ayush-mishra5/'
-whatsapp_number: 
+whatsapp_number: 9670829546
 pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/865/original/samsung.webp?1715694954'
 post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/866/original/microsoft_%281%29.webp?1715695045'
-hours_attended: 
-problems_solved: 
+hours_attended: 56+
+problems_solved: 237+
 mentor_details: [
     {
-        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/859/original/namanBhalla_%281%29.webp?1715691506',
-        name: 'Naman Bhalla',
-        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585',
-        topic: 'Taught LLD'
-      },
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/977/original/pragy_%281%29.webp?1715768351',
+    name: 'Pragy Agarwal',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/978/original/mediaNet.webp?1715768506',
+    topic: 'Taught LLD'
+  },
       {
         image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/861/original/anshuman_%281%29.webp?1715691886',
         name: 'Anshuman Singh',
