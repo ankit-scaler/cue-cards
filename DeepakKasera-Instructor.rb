5408c6b3b8c5ad2f29670aa68423392ed62bd1f0
sksusha8853 <sksusha8853@gmail.com>
2024-05-15 21:53:28 +0530

    Completed Deepak Kasera Profile

---
+++ DeepakKasera-Instructor.rb
@@ -0,0 +1,50 @@
+---
+instructor_name: Deepak Kasera
+instructor_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/074/031/original/deepak.webp?1715789350'
+class_rating: 4.79
+location: Gurgaon
+card_type: instructor_card
+linkedin_url: 'https://www.linkedin.com/in/deepak-kasera-96897a113/'
+introduction_bio: "Instructor at Scaler.
+Ex-Senior Software Engineer, Arcesium.
+Ex-Software Development Engineer, Amazon.
+Ex-Software Engineer, Samsung."
+num_learners: 16155
+hour_classes: 2345
+work_history: [
+    {
+        "company": "Scaler",
+        "title": 'Software Engineer & Instructor',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/742/original/scaler.webp?1715593070"
+    },
+    {
+        "company": "Amazon",
+        "title": 'Senior Software Engineer',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/074/032/original/amazon_%281%29.webp?1715789894"
+    },
+    {
+        "company": "Arcesium",
+        "title": 'Software Engineer',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/074/033/original/arcesium.webp?1715790016"
+    },
+    {
+        "company": "Samsung",
+        "title": 'Software Engineer',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/074/034/original/samsung_%281%29.webp?1715790088"
+    },
+]
+students_working_at_company_logos: [
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/745/original/google.webp?1715593708",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/746/original/amazon.webp?1715593782",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/747/original/microsoft.webp?1715593910",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/748/original/swiggy.webp?1715594132",
+
+]
+---
+
+# Skill Tags
+- System Design Expert
+- DSA Expert
+- Java Expert
+- Machine Learning Expert
+
