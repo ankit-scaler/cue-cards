3a6f703d6aab828c69b7a5601a2294572182fe70
sksusha8853 <sksusha8853@gmail.com>
2024-05-17 01:41:04 +0530

    Akhil full profile done, Pulkit image is pending

---
+++ AkhilKumarAlampally-Alumni.rb
@@ -0,0 +1,36 @@
+---
+alumni_name: Akhil Kumar Alampally
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/074/169/original/aka.webp?1715887757'
+role: Software Engineer
+batch: Software Development June 2021
+location: Hyderabad
+card_type: alumni_card
+linkedin_url: 'https://www.linkedin.com/in/akhil-kumar-alampally/'
+whatsapp_number: 9582025588
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/074/170/original/adp.webp?1715888179'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/074/171/original/credera.webp?1715888290'
+hours_attended: 382
+problems_solved: 208
+mentor_details: [
+    {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/859/original/namanBhalla_%281%29.webp?1715691506',
+        name: 'Naman Bhalla',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585',
+        topic: 'Taught LLD'
+      },
+      {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/861/original/anshuman_%281%29.webp?1715691886',
+        name: 'Anshuman Singh',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/862/original/scaler_%281%29.webp?1715692250',
+        topic: 'Taught HLD'
+      },
+  
+]
+additional_skills_count: 28
+salary_hike: 150
+---
+
+# Skills
+- Full-Stack Development
+- Product Management
+- Business Process Improvement
---
+++ PulkitAneja-Instructor.rb
@@ -0,0 +1,47 @@
+---
+instructor_name: Pulkit Aneja
+instructor_image_url: 
+class_rating: 4.67
+location: Bengaluru
+card_type: instructor_card
+linkedin_url: 'https://www.linkedin.com/in/pulkit-aneja-25b327a6/'
+introduction_bio: "Top-rated Data Science instructor at Scaler. 
+Ex-Data Scientist at Amazon.
+Ex- Data Scientist-II at Meesho."
+num_learners: 5961
+hour_classes: 813
+work_history: [
+    {
+        "company": "Scaler",
+        "title": 'Data Scientist',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/742/original/scaler.webp?1715593070"
+    },
+    {
+        "company": "Meesho",
+        "title": 'Data scientist 2',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/074/172/original/meesho.webp?1715889786"
+    },
+    {
+        "company": "Amazon",
+        "title": 'Data Scientist',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/074/174/original/amazon_%282%29.webp?1715889945"
+    },
+    {
+        "company": "Scienaptic Systems",
+        "title": 'Data Scientist',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/074/173/original/Scienaptic_Systems.webp?1715889873"
+    },
+]
+students_working_at_company_logos: [
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/745/original/google.webp?1715593708",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/746/original/amazon.webp?1715593782",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/747/original/microsoft.webp?1715593910",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/748/original/swiggy.webp?1715594132",
+
+]
+---
+
+# Skill Tags
+- Machine Learning Expert
+- SQL Expert
+- Python Expert
