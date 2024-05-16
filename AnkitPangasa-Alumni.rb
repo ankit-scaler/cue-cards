37e32add6fdeed3e362dc814d26cce6b8af77706
sksusha8853 <sksusha8853@gmail.com>
2024-05-17 00:36:04 +0530

    Completed Ankit Pangasa profile

---
+++ AnkitPangasa-Alumni.rb
@@ -0,0 +1,36 @@
+---
+alumni_name: Ankit Pangasa
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/074/167/original/ankitpangasa.webp?1715886330'
+role: Senior Software Engineer
+batch: Software Development September 2021
+location: Delhi
+card_type: alumni_card
+linkedin_url: 'https://www.linkedin.com/in/ankit-pangasa/'
+whatsapp_number: 9818665087
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/074/164/original/adobe.webp?1715885727'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/074/165/original/google_%282%29.webp?1715885818'
+hours_attended: 363
+problems_solved: 309 
+mentor_details: [
+    {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/859/original/namanBhalla_%281%29.webp?1715691506',
+        name: 'Naman Bhalla',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585',
+        topic: 'Taught LLD'
+      },
+      {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/861/original/anshuman_%281%29.webp?1715691886',
+        name: 'Anshuman Singh',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/862/original/scaler_%281%29.webp?1715692250',
+        topic: 'Taught HLD'
+      },
+  
+]
+additional_skills_count: 18
+---
+
+# Skills
+- OOP
+- .NET
+- Java
+- Apache Kafka
