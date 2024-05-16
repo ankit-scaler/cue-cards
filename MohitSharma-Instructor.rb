58e89473d394a3a97e207ffcb0e1e95ce1ea9213
sksusha8853 <sksusha8853@gmail.com>
2024-05-17 00:05:00 +0530

    Completed Mohit Sharma profile

---
+++ MohitSharma-Instructor.rb
@@ -0,0 +1,36 @@
+---
+instructor_name: Mohit Sharma
+instructor_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/074/158/original/mohitsharma.webp?1715884066'
+class_rating: 4.8
+location: Delhi
+card_type: instructor_card
+linkedin_url: 'https://www.linkedin.com/in/mohit-sharma/'
+introduction_bio: "Software Engineer and Lead Instructor at Scaler.
+Ex-Software Engineer at Wingify."
+num_learners: 7571
+hour_classes: 2028
+work_history: [
+    {
+        "company": "Scaler",
+        "title": 'SDE and Lead Instructor',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/742/original/scaler.webp?1715593070"
+    },
+    {
+        "company": "Wingify",
+        "title": 'Software Engineer',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/074/160/original/wingify.webp?1715884361"
+    },
+]
+students_working_at_company_logos: [
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/745/original/google.webp?1715593708",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/746/original/amazon.webp?1715593782",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/747/original/microsoft.webp?1715593910",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/748/original/swiggy.webp?1715594132",
+
+]
+---
+
+# Skill Tags
+- System Design Expert
+- SQL Expert
+- DSA Expert
