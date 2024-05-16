99f91b6dd362044085927bc09d5e45d8d31bf98b
sksusha8853 <sksusha8853@gmail.com>
2024-05-17 01:58:30 +0530

    Nikhil profile without image url

---
+++ NikhilSanghi-Instructor.rb
@@ -0,0 +1,37 @@
+---
+instructor_name: Nikhil Sanghi
+instructor_image_url: 
+class_rating: 4.74
+location: Rajasthan
+card_type: instructor_card
+linkedin_url: 'https://www.linkedin.com/in/nikhil-sanghi-8a8606a5/'
+introduction_bio: "Lead Instructor for DSML at Scaler.
+Co-founder of Stonkr.
+"
+num_learners: 6029
+hour_classes: 1190
+work_history: [
+    {
+        "company": "Scaler",
+        "title": 'Lead Data Scientist and Instructor',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/742/original/scaler.webp?1715593070"
+    },
+    {
+        "company": "Stonkr",
+        "title": 'Co-Founder',
+        "company_logo_url": ""
+    },
+]
+students_working_at_company_logos: [
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/745/original/google.webp?1715593708",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/746/original/amazon.webp?1715593782",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/747/original/microsoft.webp?1715593910",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/748/original/swiggy.webp?1715594132",
+
+]
+---
+
+# Skill Tags
+- Machine Learning Expert
+- Python Expert
+- DSA Expert
