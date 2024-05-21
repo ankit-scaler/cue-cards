e8ba3c0ba2931ccfdcd724098e22aa57b2a8ad96
sksusha8853 <sksusha8853@gmail.com>
2024-05-22 00:16:34 +0530

    Added complete Akshat Arora profile

---
+++ AkshatArora-Alumni.rb
@@ -0,0 +1,34 @@
+---
+alumni_name: Akshat Arora
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/074/556/original/akshatarora.webp?1716316619'
+role: AI/ML Scientist 
+batch: Data Science & ML March 2023
+location: Bangalore
+card_type: alumni_card
+linkedin_url: 'https://www.linkedin.com/in/akshat-arora-917014801401akki/'
+whatsapp_number: 9928004454
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/074/557/original/hcl_%281%29.webp?1716316996'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/074/558/original/mindshare.webp?1716317063'
+hours_attended: 93
+problems_solved: 176
+mentor_details: [
+    {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/982/original/shivank_%281%29.webp?1715769654',
+        name: 'Shivank Agarwal',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/983/original/microsoft_%281%29_%282%29.webp?1715769840',
+        topic: 'Taught enerative AI'
+      },
+      {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/985/original/surraj_%282%29.webp?1715770309',
+        name: 'Suraaj Hasija',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/986/original/mastercard_%281%29.webp?1715770428',
+        topic: 'Taught Business Case'
+      },
+  
+]
+additional_skills_count: 45
+---
+
+# Skills
+- Predictive Modeling
+- Statistical Modeling
