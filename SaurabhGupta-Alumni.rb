6a61a6f7959c55fd4d8410c3cf3698b1634dbdd9
sksusha8853 <sksusha8853@gmail.com>
2024-05-22 23:21:36 +0530

    Added complete profile of Saurabh Gupta

---
+++ SaurabhGupta-Alumni.rb
@@ -0,0 +1,34 @@
+---
+alumni_name: Saurabh Gupta
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/074/645/original/Saurabh_Gupta.webp?1716399708'
+role: Research NLP Engineer ML
+batch: Data Science & ML May 2022
+location: Mira Bhayandar Road
+card_type: alumni_card
+linkedin_url: 'https://www.linkedin.com/in/gupta-saurabh-mentor/'
+whatsapp_number: 7208729629
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/074/647/original/Mphasis.webp?1716399857'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/074/649/original/predii.webp?1716400019'
+hours_attended: 166
+problems_solved: 808 
+mentor_details: [
+    {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/982/original/shivank_%281%29.webp?1715769654',
+        name: 'Shivank Agarwal',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/983/original/microsoft_%281%29_%282%29.webp?1715769840',
+        topic: 'Taught DSA'
+      },
+      {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/985/original/surraj_%282%29.webp?1715770309',
+        name: 'Suraaj Hasija',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/986/original/mastercard_%281%29.webp?1715770428',
+        topic: 'Taught Business Case'
+      },
+  
+]
+additional_skills_count: 18
+---
+
+# Skills
+- TensorFlow
+- Machine Learning
