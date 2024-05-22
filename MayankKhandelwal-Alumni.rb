6900cde9fdc8fdba269926be1e88066c73c9a1df
sksusha8853 <sksusha8853@gmail.com>
2024-05-22 23:05:34 +0530

    Added complete profile of Mayank Khandelwal

---
+++ MayankKhandelwal-Alumni.rb
@@ -0,0 +1,36 @@
+---
+alumni_name: Mayank Khandelwal
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/074/638/original/MayankKhandewal.webp?1716398705'
+role: Computer Vision Engineer
+batch: Data Science & ML Oct-21
+location: Pune
+card_type: alumni_card
+linkedin_url: 'https://www.linkedin.com/in/khmayank/'
+whatsapp_number: 9445608113
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/074/639/original/tcs.webp?1716398919'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/074/640/original/camcom.webp?1716398996'
+hours_attended: 949
+problems_solved: 901
+mentor_details: [
+    {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/982/original/shivank_%281%29.webp?1715769654',
+        name: 'Shivank Agarwal',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/983/original/microsoft_%281%29_%282%29.webp?1715769840',
+        topic: 'Taught DSA'
+      },
+      {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/985/original/surraj_%282%29.webp?1715770309',
+        name: 'Suraaj Hasija',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/986/original/mastercard_%281%29.webp?1715770428',
+        topic: 'Taught Business Case'
+      },
+   
+  
+]
+additional_skills_count: 50
+salary_hike: 95%
+---
+
+# Skills
+- Python
+- Angular
