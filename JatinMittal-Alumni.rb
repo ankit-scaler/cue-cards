49f9005a57d2733700f08e9c36dcb3b600a7ea2f
sksusha8853 <sksusha8853@gmail.com>
2024-05-29 12:03:13 +0530

    Added Jatin Mittal Complete profile

---
+++ JatinMittal-Alumni.rb
@@ -0,0 +1,34 @@
+---
+alumni_name: Jatin Mittal
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/075/394/original/Jatin_Mittal.webp?1716963934'
+role: Senior IOS Developer
+batch: Software Development November 2021
+location: Delhi
+card_type: alumni_card
+linkedin_url: 'https://www.linkedin.com/in/jatin-mittal-88697617a/'
+whatsapp_number: +91-8700838071
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/075/395/original/craterzone.webp?1716964212'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/075/396/original/paytm_%281%29.webp?1716964267'
+hours_attended: 240
+problems_solved: 231
+mentor_details: [
+    {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/859/original/namanBhalla_%281%29.webp?1715691506',
+        name: 'Naman Bhalla',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585',
+        topic: 'Taught LLD'
+      },
+      {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/861/original/anshuman_%281%29.webp?1715691886',
+        name: 'Anshuman Singh',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/862/original/scaler_%281%29.webp?1715692250',
+        topic: 'Taught HLD'
+      },
+  
+]
+additional_skills_count: 11
+---
+
+# Skills
+- Swift
+- DSA
