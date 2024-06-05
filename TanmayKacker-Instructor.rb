e0bbbab6ba66cbfc90886e0fcbc4b4615ed7bb9f
Adarsh Kumar Shrivastav <adarsh.kumar@scaler.com>
2024-06-05 12:21:57 +0530

    Update TanmayKacker-Instructor.rb

--- TanmayKacker-Instructor.rb
+++ TanmayKacker-Instructor.rb
@@ -1,6 +1,6 @@
 ---
 instructor_name: Tanmay Kacker
-instructor_image_url: "https://avatars.githubusercontent.com/u/6458661?v=4"
+instructor_image_url: "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/076/484/original/TanmayKacker_Instructorw.webp?1717569679"
 class_rating: 4.71
 location: Bengaluru, KA, India
 card_type: instructor_card
@@ -14,19 +14,19 @@ work_history: [
     {
         "company": "Locus",
         "title": 'Tech Lead',
-        "company_logo_url": "https://locus.sh/assets/img/logo/locus-white.svg"
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/076/485/original/locusw.webp?1717569825"
     },
     {
         "company": "Practo",
         "title": 'Senior Software Developer',
-        "company_logo_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSV9toIconMh1zl2vB7_5QYzgXO7pu2iTz2IzI4Jtdu&s"
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/076/486/original/practow.webp?1717569918"
     },
 ]
 students_working_at_company_logos: [
-    "https://pbs.twimg.com/profile_images/1440775265928417284/ywTBFW6L_400x400.png",
-    'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/071/880/original/0a06600cc3cedeb49280b54114c88ce6.jpg?1713936860',
-    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/071/881/original/swiggy-1.svg?1713936920",
-    "https://pbs.twimg.com/profile_images/1408089447455891461/hwvI8tM1_400x400.jpg",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/076/487/original/googlew.webp?1717569999",
+    'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/076/488/original/amazonw.webp?1717570065',
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/076/489/original/swiggyw.webp?1717570138",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/076/491/original/mswindowsw.webp?1717570267",
 ]
 ---
 
