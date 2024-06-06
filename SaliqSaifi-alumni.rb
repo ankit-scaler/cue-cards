4cc2d03063682c4fc13a4d1267940164ba8937c4
Adarsh Kumar Shrivastav <adarsh.kumar@scaler.com>
2024-06-06 11:24:32 +0530

    Create SaliqSaifi-alumni.rb
    Created By Adarsh

---
+++ SaliqSaifi-alumni.rb
@@ -0,0 +1,36 @@
+---
+alumni_name: Saliq Saifi
+alumni_image_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/076/744/original/SaliqSaifiw.webp?1717652834
+role: Application Developer 3
+batch: Software Development November 2021
+location: New Delhi
+card_type: alumni_card
+linkedin_url: https://www.linkedin.com/in/saliq97/
+whatsapp_number: +91-9690755259
+pre_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/076/746/original/mahindracomvivaw.webp?1717653089
+post_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/076/747/original/oraclew.webp?1717653156
+hours_attended: 736
+problems_solved: 232
+mentor_details: [
+      {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/859/original/namanBhalla_%281%29.webp?1715691506',
+        name: 'Naman Bhalla',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585',
+        topic: 'Taught LLD'
+      },
+      {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/861/original/anshuman_%281%29.webp?1715691886',
+        name: 'Anshuman Singh',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/862/original/scaler_%281%29.webp?1715692250',
+        topic: 'Taught HLD'
+      },
+  
+]
+additional_skills_count: 24
+salary_hike: 500%
+---
+
+# Skills
+- Python
+- SQL
+- Data Structure
