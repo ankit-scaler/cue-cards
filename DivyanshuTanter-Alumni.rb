5e2d1d840997cda61a3b21310b6e452962a955a5
Adarsh Kumar Shrivastav <adarsh.kumar@scaler.com>
2024-06-18 10:17:23 +0530

    Create DivyanshuTanter-Alumni.rb
    Created By Adarsh

---
+++ DivyanshuTanter-Alumni.rb
@@ -0,0 +1,37 @@
+---
+alumni_name: Divyanshu Tanter
+alumni_image_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/078/413/original/DivyanshuTanterw.webp?1718684890
+role: AI R&D
+batch: Data Science & ML May 2022
+location: Bangalore, KA, India
+card_type: alumni_card
+linkedin_url: https://www.linkedin.com/in/divyanshu-tanter-353795179/
+whatsapp_number: +91-8755245597	
+pre_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/078/414/original/wiprow.webp?1718685600
+post_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/078/415/original/Dassault_Syst%C3%A8mes-Logo.winew.webp?1718685746
+hours_attended: 381
+problems_solved: 717
+mentor_details: [
+      {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/982/original/shivank_%281%29.webp?1715769654',
+        name: 'Shivank Agarwal',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/983/original/microsoft_%281%29_%282%29.webp?1715769840',
+        topic: 'Taught Generative AI'
+      },
+      {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/985/original/surraj_%282%29.webp?1715770309',
+        name: 'Suraaj Hasija',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/986/original/mastercard_%281%29.webp?1715770428',
+        topic: 'Taught Business Case'
+      },
+  
+]
+additional_skills_count: 27
+salary_hike: 250%
+---
+
+# Skills
+- Python
+- Oracle SQL
+- Deep Learning
+- Object-Oriented Programming(OOP)
