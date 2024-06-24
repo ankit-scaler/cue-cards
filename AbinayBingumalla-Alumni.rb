bac760953f8e9ded10b6521bfef42fe22f614a8d
Adarsh Kumar Shrivastav <adarsh.kumar@scaler.com>
2024-06-24 14:18:18 +0530

    Update AbinayBingumalla-Alumni.rb
    Updated By Adarsh

--- AbinayBingumalla-Alumni.rb
+++ AbinayBingumalla-Alumni.rb
@@ -1,28 +1,28 @@
 ---
 alumni_name: Abinay Bingumalla
-alumni_image_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/016/284/original/success-story-image_id.png?1665563964
+alumni_image_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/079/194/original/Abhinayw.webp?1719218528
 role: Full Stack
 batch: January 20 EliteX
 location: Bangalore
 card_type: alumni_card
 linkedin_url: https://www.linkedin.com/in/abinayb/
 whatsapp_number: 9703535151
-pre_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/072/197/original/relianceJio.png?1714070781
-post_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/072/196/original/microsoft.png?1714069933
+pre_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/079/195/original/reliancew.webp?1719218620
+post_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/079/196/original/msw.webp?1719218716
 hours_attended: 104.46
 problems_solved: 350
 mentor_details: [
     {
-    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/071/877/original/Shivank_2.png?1713936614',
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/079/197/original/Prateekw.webp?1719218837',
     name: 'Prateek Narang',
-    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/071/878/original/google_1.svg?1713936640',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585,
     topic: 'Taught Abinay DSA'
   },
   {
-    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/071/879/original/Naman_Bhalla_02_5.png?1713936684',
-    name: 'Naman Bhalla',
-    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/071/878/original/google_1.svg?1713936640',
-    topic: 'Taught Abinay HLD/LLD'
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/859/original/namanBhalla_%281%29.webp?1715691506',
+        name: 'Naman Bhalla',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585',
+        topic: 'Taught Abhinay LLD'
   }
  
 ]
