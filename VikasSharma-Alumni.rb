75a77337fa912d5da5904a49ad10707dc670d595
Adarsh Kumar Shrivastav <adarsh.kumar@scaler.com>
2024-06-27 10:02:47 +0530

    Update VikasSharma-Alumni.rb
    Updated by Adarsh

--- VikasSharma-Alumni.rb
+++ VikasSharma-Alumni.rb
@@ -1,21 +1,21 @@
 ---
 alumni_name: Vikas Sharma
-alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/017/795/original/success-story-image_id.png?1665567522'
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/079/427/original/vikas_mittalw.webp?1719462185'
 role: SDE 3
 batch: Academy Dec21
 location: Ahmedabad
 card_type: alumni_card
 linkedin_url: 'https://www.linkedin.com/in/vikas-sharma-09650b143/'
 whatsapp_number: 8320265854
-pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/089/original/Bacancy_Technology.png?1715087570'
-post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/091/original/walmart.png?1715087652'
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/079/429/original/bacancyw.webp?1719462343'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/079/430/original/walmart1w.webp?1719462441'
 hours_attended: 386.78
 problems_solved: 251
 mentor_details: [
   {
-    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/325/original/Abhinav_Dubey___Software_%28SDE_2%29___Amazon.png?1715238993',
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/079/431/original/abhinav_dubeyw.webp?1719462600',
     name: 'Abhinav Dubey',
-    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/071/880/original/0a06600cc3cedeb49280b54114c88ce6.jpg?1713936860',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/079/433/original/amazon1w.webp?1719462743',
     topic: 'Taught Vikas DSA'
   },
   
