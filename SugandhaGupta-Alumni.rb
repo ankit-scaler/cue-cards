e390e6f2e1273646c28235e9e28069cc575f2468
Adarsh Kumar Shrivastav <adarsh.kumar@scaler.com>
2024-06-28 14:31:39 +0530

    Create SugandhaGupta-Alumni.rb
    Created By Adarsh

---
+++ SugandhaGupta-Alumni.rb
@@ -0,0 +1,36 @@
+---
+alumni_name: Sugandha Gupta
+alumni_image_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/079/653/original/Sugandha_Guptaw.webp?1719564041
+role: Senior data analyst
+batch: Data Science & ML March 2022
+location: Delhi
+card_type: alumni_card
+linkedin_url: https://www.linkedin.com/in/sugandha-gupta-4b5a89234/
+whatsapp_number: +91-8295134070	
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/079/655/original/todaytomorroww.webp?1719564855'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/079/656/original/mialow.webp?1719564945'
+hours_attended: 441
+problems_solved: 1247
+mentor_details: [
+      {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/982/original/shivank_%281%29.webp?1715769654',
+        name: 'Shivank Agarwal',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/983/original/microsoft_%281%29_%282%29.webp?1715769840',
+        topic: 'Taught Python'
+      },
+      {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/985/original/surraj_%282%29.webp?1715770309',
+        name: 'Suraaj Hasija',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/986/original/mastercard_%281%29.webp?1715770428',
+        topic: 'Taught Business Case'
+      },
+  
+]
+additional_skills_count: 18
+salary_hike: 
+---
+
+# Skills
+- Python
+- SQL
+- Machine Learning
