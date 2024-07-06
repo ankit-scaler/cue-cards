73c566afcf522868494f3fb349ccbdae2ce10cbf
Adarsh Kumar Shrivastav <adarsh.kumar@scaler.com>
2024-07-06 09:05:29 +0530

    Update MandarManoharChavan-Alumni.rb
    Updated all images by Adarsh

--- MandarManoharChavan-Alumni.rb
+++ MandarManoharChavan-Alumni.rb
@@ -1,14 +1,14 @@
 ---
 alumni_name: Mandar Manohar Chavan
-alumni_image_url: 
+alumni_image_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/080/617/original/mandarw.webp?1720236899
 role: Business Analyst
 batch: Dec22 Scaler Batch
 location: Mumbai
 card_type: alumni_card
 linkedin_url: 'https://www.linkedin.com/in/mandar-chavan-856b34220/'
 whatsapp_number: 
-pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/072/192/original/healthPrime.jpeg?1714068812'
-post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/072/193/original/QuantumPhinance.png?1714068903'
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/080/615/original/healthPrimew.webp?1720236564'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/080/616/original/QuantumPhinancew.webp?1720236643'
 hours_attended: 
 problems_solved: 
 mentor_details: [
