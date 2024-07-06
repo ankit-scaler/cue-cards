de15adeba6f8f08aedc69e6542267f2281cd2fc5
Adarsh Kumar Shrivastav <adarsh.kumar@scaler.com>
2024-07-06 10:14:13 +0530

    Update MandarManoharChavan-Alumni.rb
    Updated By Adarsh

--- MandarManoharChavan-Alumni.rb
+++ MandarManoharChavan-Alumni.rb
@@ -6,13 +6,24 @@ batch: Dec22 Scaler Batch
 location: Mumbai
 card_type: alumni_card
 linkedin_url: 'https://www.linkedin.com/in/mandar-chavan-856b34220/'
-whatsapp_number: 
+whatsapp_number: +91-8450919484
 pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/080/615/original/healthPrimew.webp?1720236564'
 post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/080/616/original/QuantumPhinancew.webp?1720236643'
-hours_attended: 
-problems_solved: 
+hours_attended: 453
+problems_solved: 600
 mentor_details: [
-  
+      {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/982/original/shivank_%281%29.webp?1715769654',
+        name: 'Shivank Agarwal',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/983/original/microsoft_%281%29_%282%29.webp?1715769840',
+        topic: 'Taught Advance DSA'
+      },
+      {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/985/original/surraj_%282%29.webp?1715770309',
+        name: 'Suraaj Hasija',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/986/original/mastercard_%281%29.webp?1715770428',
+        topic: 'Taught Business Case'
+      },
 ]
 additional_skills_count: 28+
 salary_hike: 
