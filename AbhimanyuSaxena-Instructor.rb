0dbba4736ee6dfadc96e860ffe3b4f5d454f0024
Adarsh Kumar Shrivastav <adarsh.kumar@scaler.com>
2024-07-07 21:11:43 +0530

    Update AbhimanyuSaxena-Instructor.rb
    Updated By Adarsh

--- AbhimanyuSaxena-Instructor.rb
+++ AbhimanyuSaxena-Instructor.rb
@@ -1,35 +1,35 @@
 ---
 instructor_name: Abhimanyu Saxena
-instructor_image_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/071/821/original/image_24.png
+instructor_image_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/080/755/original/abhimanyuw.webp?1720366784
 class_rating: 4.7
 location: Bangalore
 card_type: instructor_card
 linkedin_url: 'https://www.linkedin.com/in/abhimanyusaxena/?originalSubdomain=in'
-introduction_bio: Abhimanyu is seasoned SDE with over a decade at Amazon and Google, mastering system design via HLD & implementing complex algorithms & data structures. Bridges academia and industry with practical insights into applying HLD & DSA principles.
+introduction_bio: Abhimanyu is a seasoned SDE with over a decade at Amazon and Google, mastering system design via HLD & implementing complex algorithms & data structures. Bridges academia and industry with practical insights into applying HLD & DSA principles.
 num_learners: 3,565+
 hour_classes: 1,234+
 work_history: [
     {
       "company": "Scaler",
       "title": 'Instructor, Co-Founder',
-      "company_logo_url": "https://pbs.twimg.com/profile_images/1491663604487102465/_jWKH2Gt_400x400.jpg"
+      "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/742/original/scaler.webp?1715593070"
     },
     {
       "company": "Google",
       "title": "Senior Software Engineer",
-      "company_logo_url": "https://pbs.twimg.com/profile_images/1440775265928417284/ywTBFW6L_400x400.png"
+      "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/751/original/google.webp?1715597034"
     },
     {
       "company": "Amazon",
       "title": "Software Engineer",
-      "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/071/880/original/0a06600cc3cedeb49280b54114c88ce6.jpg?1713936860"
+      "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/074/032/original/amazon_%281%29.webp?1715789894"
     }
 ]
 students_working_at_company_logos: [
-    "https://pbs.twimg.com/profile_images/1440775265928417284/ywTBFW6L_400x400.png",
-    'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/071/880/original/0a06600cc3cedeb49280b54114c88ce6.jpg?1713936860',
-    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/071/881/original/swiggy-1.svg?1713936920",
-    "https://pbs.twimg.com/profile_images/1408089447455891461/hwvI8tM1_400x400.jpg",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/745/original/google.webp?1715593708",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/746/original/amazon.webp?1715593782",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/747/original/microsoft.webp?1715593910",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/748/original/swiggy.webp?1715594132",
 ]
 ---
 
