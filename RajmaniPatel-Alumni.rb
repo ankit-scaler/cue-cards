0fffcd8e3169a942fde686a07dede39a4263472c
Adarsh Kumar Shrivastav <adarsh.kumar@scaler.com>
2024-07-08 07:46:21 +0530

    Create RajmaniPatel-Alumni.rb
    Created By Adarsh

---
+++ RajmaniPatel-Alumni.rb
@@ -0,0 +1,36 @@
+---
+alumni_name: Rajmani Patel
+alumni_image_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/080/802/original/RajmaniPatelw.webp?1720404453
+role: Senior Software Engineer at Microsoft
+batch: Software Development January 2021
+location: Bangalore
+card_type: alumni_card
+linkedin_url: https://www.linkedin.com/in/rajmani-patel-14009616/
+whatsapp_number: +91-8553142433
+pre_scaler_org_logo_url: "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/080/803/original/vmwarew.webp?1720404719"
+post_scaler_org_logo_url: "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/080/804/original/microsoftw.webp?1720404787"
+hours_attended: 277
+problems_solved: 208
+mentor_details: [
+  {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/859/original/namanBhalla_%281%29.webp?1715691506',
+    name: 'Naman Bhalla',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585',
+    topic: 'Taught LLD'
+  },
+  {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/861/original/anshuman_%281%29.webp?1715691886',
+    name: 'Anshuman Singh',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/862/original/scaler_%281%29.webp?1715692250',
+    topic: 'Taught HLD'
+  },
+]
+additional_skills_count: 28
+salary_hike: 
+---
+
+# Skills
+- Java
+- SQL
+- Data Structures
+- JavaScript
