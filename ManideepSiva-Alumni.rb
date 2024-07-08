32fd72be539f0bbb5b90732620483e44f1472826
Adarsh Kumar Shrivastav <adarsh.kumar@scaler.com>
2024-07-08 07:34:22 +0530

    Create ManideepSiva-Alumni.rb
    Created By Adarsh

---
+++ ManideepSiva-Alumni.rb
@@ -0,0 +1,37 @@
+---
+alumni_name: Manideep Siva
+alumni_image_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/080/799/original/ManideepSivaw.webp?1720403393
+role: Software Engineer at Arcesium
+batch: Software Development April 2020
+location: Hyderabad
+card_type: alumni_card
+linkedin_url: https://www.linkedin.com/in/manideep-siva/
+whatsapp_number: +91-9502652652
+pre_scaler_org_logo_url: "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/080/800/original/tcsw.webp?1720403671"
+post_scaler_org_logo_url: "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/080/801/original/arcesiumw.webp?1720403745"
+hours_attended: 166
+problems_solved: 325
+mentor_details: [
+      {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/977/original/pragy_%281%29.webp?1715768351',
+        name: 'Pragy Agarwal',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/978/original/mediaNet.webp?1715768506',
+        topic: 'Taught LLD'
+      },
+      {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/861/original/anshuman_%281%29.webp?1715691886',
+        name: 'Anshuman Singh',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/862/original/scaler_%281%29.webp?1715692250',
+        topic: 'Taught HLD'
+      },
+  
+]
+additional_skills_count: 66
+salary_hike: 
+---
+
+# Skills
+- Python
+- SQL
+- Low Level Design
+- Data Structures
