870ef026cca21324f5a098eb4765e081d324fab9
Adarsh Kumar Shrivastav <adarsh.kumar@scaler.com>
2024-07-18 12:10:41 +0530

    Create SandeepSinha-Instructor.rb
    Created By Adarsh

---
+++ SandeepSinha-Instructor.rb
@@ -0,0 +1,35 @@
+---
+instructor_name: Sandeep Sinha
+instructor_image_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/082/227/original/SandeepSinha2w.webp?1721283546
+class_rating: 4.72
+location: Bengaluru, KA
+card_type: instructor_card
+linkedin_url: "https://www.linkedin.com/in/7sandeepsinha/"
+introduction_bio: "Sandeep is a Software Engineer at Atlassian, specializing in backend development for Jira Service Management with expertise in Java 17, Spring Boot 2, AWS SQS, and Dynamo DB. He has prior experience at Visa, AJIO.com, and Capgemini."
+num_learners: 7421
+hour_classes: 1418
+work_history: [
+    {
+        "company": "Atlassian",
+        "title": 'Software Engineer',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/082/228/original/atlassianw.webp?1721284280"
+    },
+    {
+        "company": "Visa",
+        "title": 'Senior Software Engineer',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/082/229/original/visaw.webp?1721284433"
+    },
+]
+students_working_at_company_logos: [
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/745/original/google.webp?1715593708",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/746/original/amazon.webp?1715593782",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/747/original/microsoft.webp?1715593910",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/748/original/swiggy.webp?1715594132",
+
+]
+---
+
+# Skill Tags
+- DSA Expert
+- Java Expert
+- SQL Expert
