116b0ee3467e5d3df79a3c87836bea436d94a1bb
Adarsh Kumar Shrivastav <adarsh.kumar@scaler.com>
2024-07-24 11:03:02 +0530

    Create PriyankaNabapure-Alumni.rb
    Created By Adarsh

---
+++ PriyankaNabapure-Alumni.rb
@@ -0,0 +1,37 @@
+---
+alumni_name: Priyanka Nabapure
+alumni_image_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/082/846/original/PriyankaNabapurew.webp?1721797592
+role: Senior Software Developer
+batch: Software Development December 2022
+location: Bengaluru, KA
+card_type: alumni_card
+linkedin_url: https://www.linkedin.com/in/pnabapure/
+whatsapp_number: +91-7022439125
+pre_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/082/847/original/Meghcomputingw.webp?1721797893
+post_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/082/848/original/cme_groupw.webp?1721798046
+hours_attended: 279
+problems_solved: 366
+mentor_details: [
+  {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/859/original/namanBhalla_%281%29.webp?1715691506',
+    name: 'Naman Bhalla',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585',
+    topic: 'Taught LLD'
+  },
+  {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/861/original/anshuman_%281%29.webp?1715691886',
+    name: 'Anshuman Singh',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/862/original/scaler_%281%29.webp?1715692250',
+    topic: 'Taught HLD'
+  },
+  
+]
+additional_skills_count: 43
+salary_hike: 
+---
+
+# Skills
+- SpringBoot
+- LLD
+- MySQL
+- Data Structures
