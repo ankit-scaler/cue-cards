4548d81c971a9c2898ef060e10824e8f999bba78
ankit-scaler <114554881+ankit-scaler@users.noreply.github.com>
2024-07-31 13:42:01 +0530

    Create Atharva-Deshmukh-alumni.rb

---
+++ Atharva-Deshmukh-alumni.rb
@@ -0,0 +1,24 @@
+---
+alumni_name: Atharva Deshmukh
+alumni_image_url: 
+role: Software Engineer
+batch: Academy Feb23 Intermediate
+location: Mumbai
+card_type: alumni_card
+linkedin_url: https://www.linkedin.com/in/AtharvaDev/
+whatsapp_number: 
+pre_scaler_org_logo_url: 
+post_scaler_org_logo_url: 
+hours_attended: 560
+problems_solved: 416
+mentor_details: [
+  
+  
+]
+additional_skills_count: 7
+salary_hike: 
+---
+
+# Skills
+- Python
+- SQL
