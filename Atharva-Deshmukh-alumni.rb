1b15ed1b952e7d110e90a6a8857da0dcb5544f01
Adarsh Kumar Shrivastav <adarsh.kumar@scaler.com>
2024-08-01 08:01:39 +0530

    Update Atharva-Deshmukh-alumni.rb
    Updated by Adarsh

--- Atharva-Deshmukh-alumni.rb
+++ Atharva-Deshmukh-alumni.rb
@@ -1,24 +1,37 @@
 ---
 alumni_name: Atharva Deshmukh
-alumni_image_url: 
+alumni_image_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/084/177/original/atharvw.webp?1722478806
 role: Software Engineer
 batch: Academy Feb23 Intermediate
 location: Mumbai
 card_type: alumni_card
 linkedin_url: https://www.linkedin.com/in/AtharvaDev/
-whatsapp_number: 
-pre_scaler_org_logo_url: 
-post_scaler_org_logo_url: 
+whatsapp_number: 8850414839
+pre_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/084/178/original/NeebalTechw.webp?1722479179
+post_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/084/179/original/citiw.webp?1722479274
 hours_attended: 560
 problems_solved: 416
 mentor_details: [
-  
+  {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/859/original/namanBhalla_%281%29.webp?1715691506',
+    name: 'Naman Bhalla',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585',
+    topic: 'Taught LLD'
+  },
+  {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/861/original/anshuman_%281%29.webp?1715691886',
+    name: 'Anshuman Singh',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/862/original/scaler_%281%29.webp?1715692250',
+    topic: 'Taught HLD'
+  },
   
 ]
-additional_skills_count: 7
+additional_skills_count: 38
 salary_hike: 
 ---
 
 # Skills
-- Python
+- Java
 - SQL
+- JavaScript
+- SpringBoot
