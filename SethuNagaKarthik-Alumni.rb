a2ace3f7abaff98b22a8189cdb83484a1096dff4
Adarsh Kumar Shrivastav <adarsh.kumar@scaler.com>
2024-08-07 11:14:15 +0530

    Update SethuNagaKarthik-Alumni.rb
    Updated by Adarsh

--- SethuNagaKarthik-Alumni.rb
+++ SethuNagaKarthik-Alumni.rb
@@ -1,28 +1,28 @@
 ---
 alumni_name: SethuNagaKarthik
-alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/028/685/original/success-story-image_id.png?1679298063'
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/085/249/original/SethuNagaw.webp?1723009065'
 role: Software Development Engineer-2
 batch: Feb22 Scaler Batch
 location: Chennai
 card_type: alumni_card
 linkedin_url: 'https://www.linkedin.com/in/sethunagakarthik-fullstackdev/'
 whatsapp_number: 9566841129
-pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/085/original/trane_technologies.png?1715085595'
-post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/086/original/Airbus.jpeg?1715085812'
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/085/250/original/trane_technologiesw.webp?1723009230'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/085/251/original/Airbusw.webp?1723009303'
 hours_attended: 509.9
 problems_solved: 271
 mentor_details: [
   {
-    image: '',
-    name: 'Mandeep',
-    company_logo_url: '',
-    topic: 'Taught Sethu Java'
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/079/197/original/Prateekw.webp?1719218837',
+    name: 'Prateek Narang',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585',
+    topic: 'Taught DSA'
   },
   {
-    image: '',
-    name: 'Ashwin Bhoyar',
-    company_logo_url: '',
-    topic: 'Taught Sethu DSA'
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/859/original/namanBhalla_%281%29.webp?1715691506',
+    name: 'Naman Bhalla',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585',
+    topic: 'Taught LLD'
   }
 ]
 additional_skills_count: 50+
