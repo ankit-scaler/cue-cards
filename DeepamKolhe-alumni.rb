335accb408371cff2b85fd09d52ca844480855f9
Adarsh Kumar Shrivastav <adarsh.kumar@scaler.com>
2024-08-26 12:09:26 +0530

    Create DeepamKolhe-alumni.rb
    Created By Adarsh

---
+++ DeepamKolhe-alumni.rb
@@ -0,0 +1,37 @@
+---
+alumni_name: Deepam Kolhe
+alumni_image_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/087/485/original/DeepamKolhew.webp?1724654139
+role: SDE-1 at Amazon
+batch: Software Development October 2021
+location: Pune
+card_type: alumni_card
+linkedin_url: https://www.linkedin.com/in/deepam-kolhe-2298baa6/
+whatsapp_number: 7987508033
+pre_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/087/483/original/Tibcow.webp?1724653816
+post_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/087/484/original/amazonw225.webp?1724653911
+hours_attended: 502
+problems_solved: 118
+mentor_details: [
+  {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/859/original/namanBhalla_%281%29.webp?1715691506',
+    name: 'Naman Bhalla',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585',
+    topic: 'Taught LLD'
+  },
+  {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/861/original/anshuman_%281%29.webp?1715691886',
+    name: 'Anshuman Singh',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/862/original/scaler_%281%29.webp?1715692250',
+    topic: 'Taught HLD'
+  },
+  
+]
+additional_skills_count: 24
+salary_hike: 186%
+---
+
+# Skills
+- Java
+- Data Structures
+- MySQL
+- Python
