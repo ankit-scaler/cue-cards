010395349141cbfcea03eaeb69a94c24ec0b3a02
satyam-is-shanks <satyam.singh03@scaler.com>
2024-08-29 15:24:43 +0530

    Create SanjuSaini-Alumni.rb
    created by satyam singh

---
+++ SanjuSaini-Alumni.rb
@@ -0,0 +1,35 @@
+---
+alumni_name: Sanju Saini
+alumni_image_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/087/844/original/Sanju_Sainiw.webp?1724923518
+role: Product Development Engineer 2 at Razorpay
+batch: Software Development April 2022
+location: Alwar
+card_type: alumni_card
+linkedin_url: https://www.linkedin.com/in/coder-sanju/
+whatsapp_number: 7976495067
+pre_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/087/845/original/in_time_tec_logow.webp?1724923546
+post_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/087/846/original/amazonw.webp?1724923692
+hours_attended: 251
+problems_solved: 179
+mentor_details: [
+    {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/859/original/namanBhalla_%281%29.webp?1715691506',
+    name: 'Naman Bhalla',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585',
+    topic: 'Taught LLD'
+  },
+  {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/861/original/anshuman_%281%29.webp?1715691886',
+    name: 'Anshuman Singh',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/862/original/scaler_%281%29.webp?1715692250',
+    topic: 'Taught HLD'
+  },
+  
+]
+additional_skills_count: 54
+---
+
+# Skills
+- Golang
+- KAFKA
+- Reactjs
