5df9485ee43015ac3b0e6071e46948d4eee4bfc8
satyam-is-shanks <satyam.singh03@scaler.com>
2024-09-20 15:54:15 +0530

    Create AjayShenoy-Instructor.rb
    some updates needed will do them later ny satyam singh

---
+++ AjayShenoy-Instructor.rb
@@ -0,0 +1,30 @@
+---
+instructor_name:  Ajay Shenoy
+instructor_image_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/090/248/original/ajay_photo.webp?1726827420
+class_rating: 4.88
+location: 
+card_type: instructor_card
+linkedin_url:https://www.linkedin.com/in/bastyajayshenoy/ 
+introduction_bio: 
+num_learners: 
+hour_classes: 
+work_history: [
+    {
+        "company": "",
+        "title": '',
+        "company_logo_url": ""
+    },
+]
+students_working_at_company_logos: [
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/745/original/google.webp?1715593708",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/746/original/amazon.webp?1715593782",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/747/original/microsoft.webp?1715593910",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/748/original/swiggy.webp?1715594132",
+
+]
+---
+
+# Skill Tags
+- System Design Expert
+- DSA Expert
+- Java Expert
