28b0f8cab5da1934dc95e4d7fe9b79f6c09c20b8
satyam-is-shanks <satyam.singh03@scaler.com>
2024-09-25 23:00:10 +0530

    Update SiddharthTaneja-Instructor.rb
    updated by satyam singh

--- SiddharthTaneja-Instructor.rb
+++ SiddharthTaneja-Instructor.rb
@@ -1,12 +1,12 @@
 ---
 instructor_name: Siddharth Taneja
-instructor_image_url: "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/090/688/original/file.webp?1727265793"
+instructor_image_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/090/688/original/file.webp?1727265793
 class_rating: 4.82
 location: Bangalore
 card_type: instructor_card
-linkedin_url: "https://www.linkedin.com/in/siddharth-taneja-98596588/"
-introduction_bio: Senior Engineering Manager & Instructor at Scaler
-num_learners:31291 
+linkedin_url: 'https://www.linkedin.com/in/siddharth-taneja-98596588/'
+introduction_bio: "Senior Engineering Manager & Instructor at Scaler."
+num_learners: 31291
 hour_classes: 651.8
 work_history: [
     {
@@ -14,27 +14,26 @@ work_history: [
         "title": 'Frontend lead',
         "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/090/692/original/interviewbit_logo.webp?1727266015"
     },
-      {
+    {
         "company": "Qubole",
         "title": 'Member of Technical Staff',
         "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/090/689/original/qubole_logo.webp?1727265929"
     },
-      {
+    {
         "company": "Directi",
         "title": 'UI Developer',
         "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/090/691/original/directi_logo.webp?1727265993"
-    },
+    }
 ]
 students_working_at_company_logos: [
     "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/745/original/google.webp?1715593708",
     "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/746/original/amazon.webp?1715593782",
     "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/747/original/microsoft.webp?1715593910",
-    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/748/original/swiggy.webp?1715594132",
-
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/748/original/swiggy.webp?1715594132"
 ]
 ---
 
 # Skill Tags
-- SQL Expert 
+- SQL Expert
 - Ruby On Rails Expert
 - Javascript Expert
