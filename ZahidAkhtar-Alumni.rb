320ef19c634d54aaa8722c85b68d174759e83873
satyam-is-shanks <satyam.singh03@scaler.com>
2024-09-25 22:49:01 +0530

    Create ZahidAkhtar-Alumni.rb
    created by satyam singh rajput

---
+++ ZahidAkhtar-Alumni.rb
@@ -0,0 +1,34 @@
+---
+alumni_name: Zahid Akhtar
+alumni_image_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/090/746/original/zahid_akhtar.webp?1727284339
+role: Engineer-II at Jio
+batch: ScalerLogoSoftware Development September 2021
+location: Garhwa
+card_type: alumni_card
+linkedin_url: 'https://www.linkedin.com/in/zahid-akhtar-22b494119/'
+whatsapp_number: 7209722969
+pre_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/090/747/original/hashedin_logo.webp?1727284354
+post_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/090/748/original/jio_logo.webp?1727284367
+hours_attended: 400.35
+problems_solved: 198
+mentor_details: [
+    {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/859/original/namanBhalla_%281%29.webp?1715691506',
+    name: 'Naman Bhalla',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585',
+    topic: 'Taught LLD'
+  },
+  {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/861/original/anshuman_%281%29.webp?1715691886',
+    name: 'Anshuman Singh',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/862/original/scaler_%281%29.webp?1715692250',
+    topic: 'Taught HLD'
+  },
+]
+additional_skills_count: 22
+---
+
+# Skills
+- Java
+- Springboot
+- PostgreSQL
