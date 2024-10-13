b66bb6f076b4476fdd3fbd8d0d74444c219a990f
satyam-is-shanks <satyam.singh03@scaler.com>
2024-10-14 00:04:28 +0530

    Create ShubhamShukla-Instructor.rb
    created by satyam singh

---
+++ ShubhamShukla-Instructor.rb
@@ -0,0 +1,30 @@
+---
+instructor_name: 
+instructor_image_url: 
+class_rating: 
+location: 
+card_type: instructor_card
+linkedin_url: 
+introduction_bio: 
+num_learners: 260
+hour_classes: 17.71
+work_history: [
+    {
+        "company": "",
+        "title": '',
+        "company_logo_url": ""
+    },
+]
+students_working_at_company_logos: [
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/745/original/google.webp?1715593708",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/746/original/amazon.webp?1715593782",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/747/original/microsoft.webp?1715593910",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/748/original/swiggy.webp?1715594132",
+
+]
+---
+
+# Skill Tags
+- System Design Expert
+- DSA Expert
+- Java Expert
