b4dec14c71b7bd02ba100644cd190b20334ebca9
satyam-is-shanks <satyam.singh03@scaler.com>
2024-10-14 15:17:18 +0530

    Update ShubhamShukla-Instructor.rb
    created by satyam singh

--- ShubhamShukla-Instructor.rb
+++ ShubhamShukla-Instructor.rb
@@ -1,19 +1,29 @@
 ---
-instructor_name: 
-instructor_image_url: 
-class_rating: 
-location: 
+instructor_name: Shubham shukla
+instructor_image_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/093/639/original/shubham_shukla-removebg-preview.webp?1728898859
+class_rating: 4.51
+location: bangalore
 card_type: instructor_card
-linkedin_url: 
-introduction_bio: 
+linkedin_url: 'https://www.linkedin.com/in/snkshukla/'
+introduction_bio: "Lead DevOps at Zeta & Instructor at Scaler"
 num_learners: 260
 hour_classes: 17.71
 work_history: [
     {
-        "company": "",
-        "title": '',
-        "company_logo_url": ""
+        "company": "Zeta",
+        "title": 'DevOps lead',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/093/640/original/zeta_logo.webp?1728898898"
     },
+    {
+        "company": "Codemancer",
+        "title": 'Infrastructure solution architect',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/093/641/original/codemancer.webp?1728898914"
+    },    
+    {
+        "company": "Web developr and devOps engineer",
+        "title": 'DevOps lead',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/093/642/original/bang_the_table_logo.webp?1728898930"
+    }
 ]
 students_working_at_company_logos: [
     "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/745/original/google.webp?1715593708",
@@ -25,6 +35,7 @@ students_working_at_company_logos: [
 ---
 
 # Skill Tags
-- System Design Expert
-- DSA Expert
-- Java Expert
+- System Engineering
+- System Architecture
+- Kubernotes
+- Docker
