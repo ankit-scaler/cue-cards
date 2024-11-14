a702c3537ca6646afd05ece7797db18d5bf7da5a
satyam-is-shanks <satyam.singh03@scaler.com>
2024-11-14 18:36:40 +0530

    Update PiyushGoyal-Alumni.rb
    image updated by satyam

--- PiyushGoyal-Alumni.rb
+++ PiyushGoyal-Alumni.rb
@@ -1,6 +1,6 @@
 ---
 alumni_name: Piyush Goyal
-alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/074/025/original/piyush.webp?1715788599'
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/096/554/original/piyush_goyal_picture.webp?1731588837'
 role: Senior Software Engineer
 batch: 	Academy Apr21 Intermediate
 location: Noida
