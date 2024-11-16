ba14252408cf334f81e048eef117722414513f2f
satyam-is-shanks <satyam.singh03@scaler.com>
2024-11-16 15:48:26 +0530

    Create MadhuriKukreja-Alumni.rb
    created by satyam singh

---
+++ MadhuriKukreja-Alumni.rb
@@ -0,0 +1,37 @@
+---
+alumni_name: Madhuri Kukreja
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/096/639/original/Screenshot_2024-11-16_152226-modified.webp?1731751571'
+role: Associate Analyst at Razorpay
+batch: Data Science & ML May 2023
+location: Indore
+card_type: alumni_card
+linkedin_url: 'https://www.linkedin.com/in/madhuri-kukreja-036222172/'
+whatsapp_number: 7415213602
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/096/640/original/xl_dynamics_india_pvt_ltd_logo.webp?1731751587'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/096/641/original/razorpay_logo.webp?1731751601'
+hours_attended: 273.95
+problems_solved: 556	
+mentor_details: [
+    {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/982/original/shivank_%281%29.webp?1715769654',
+        name: 'Shivank Agarwal',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/983/original/microsoft_%281%29_%282%29.webp?1715769840',
+        topic: 'Taught Data science'
+      },
+      {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/985/original/surraj_%282%29.webp?1715770309',
+        name: 'Suraaj Hasija',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/986/original/mastercard_%281%29.webp?1715770428',
+        topic: 'Taught Generative AI'
+      },
+  
+]
+additional_skills_count: 29
+salary_hike:
+---
+
+# Skills
+- Conditional Formatting
+- Data Governance
+- MySQL
+- Python
