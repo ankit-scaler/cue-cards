04a3a7eaeb6fb25f39cf903769299cc7d49f62d5
satyam-is-shanks <satyam.singh03@scaler.com>
2024-11-19 10:39:17 +0530

    Create ShreyanshGupta-Alumni.rb
    created by satyam singh

---
+++ ShreyanshGupta-Alumni.rb
@@ -0,0 +1,29 @@
+---
+alumni_name: Shreyansh Gupta
+alumni_image_url:  'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/096/839/original/Shreyansh.webp?1731992846'
+role: SDE 1 at Amazon
+batch: Software Development May 2019
+location: Bangalore
+card_type: alumni_card
+linkedin_url: 'https://www.linkedin.com/in/shreyanshdk/'
+whatsapp_number: 7376160818
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/096/841/original/Screenshot_2024-11-19_103553.webp?1731992874'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/096/842/original/amazon.webp?1731992889'
+hours_attended: 1.12
+problems_solved: 94
+mentor_details: [
+    {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/096/837/original/abhimanyuw.webp?1731992380',
+    name: 'Abhimanyu Saxena',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/096/838/original/interviewbit_logo_%281%29.webp?1731992393',
+    topic: 'Finance Management'
+  },  
+]
+additional_skills_count: 37
+---
+
+# Skills
+- Machine Learning
+- Data Structures and Algorithm
+- Java
+- Aws
