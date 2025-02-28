b567b0587f1797141e0f8f6680e9982b253a3584
satyam-is-shanks <satyam.singh03@scaler.com>
2025-02-28 17:34:33 +0530

    Create Vignesh Raja-Alumni.rb
    created by satyam singh

---
+++ Vignesh Raja-Alumni.rb
@@ -0,0 +1,36 @@
+---
+alumni_name: Vignesh Raja
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/111/910/original/Screenshot_2025-02-28_171507.webp?1740743225'
+role: software developement engineer 2
+batch: Software Development	April 2023
+location: chennai
+card_type: alumni_card
+linkedin_url: 'https://www.linkedin.com/in/vignesh-raja-k-384571176/'
+whatsapp_number: 
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/111/912/original/01cada77a0a7d326d85b7969fe26a728.webp?1740743588'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/111/913/original/images.webp?1740743671'
+hours_attended: 515.52
+problems_solved: 384
+mentor_details: [
+   {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/859/original/namanBhalla_%281%29.webp?1715691506',
+    name: 'Naman Bhalla',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585',
+    topic: 'Taught LLD'
+  },
+  {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/861/original/anshuman_%281%29.webp?1715691886',
+    name: 'Anshuman Singh',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/862/original/scaler_%281%29.webp?1715692250',
+    topic: 'Taught HLD'
+  },
+  
+]
+additional_skills_count: 13 
+salary_hike: 120%
+---
+
+# Skills
+- Microservices
+- Data structures
+- Springboot
