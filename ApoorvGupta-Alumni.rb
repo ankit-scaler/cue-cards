87f9dbde9c47a55698ffd93d9d05547680c80993
agrim-scaler <agrim.nauriyal@scaler.com>
2025-03-07 20:44:56 +0530

    Create ApoorvGupta-Alumni.rb

---
+++ ApoorvGupta-Alumni.rb
@@ -0,0 +1,36 @@
+---
+alumni_name: Apoorv Gupta
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/112/873/original/ApoorvGupta-webp.webp?1741359053'
+role: Senior Data Scientist
+batch: Data Science & ML March 2022
+location: Delhi
+card_type: alumni_card
+linkedin_url: 'https://www.linkedin.com/in/apoorv-gupta-489008107/'
+whatsapp_number: +91-7982004322
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/112/874/original/meta-new.webp?1741359088'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/112/875/original/caastle-new.webp?1741359115'
+hours_attended: 395.33
+problems_solved: 652
+mentor_details: [
+        {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/982/original/shivank_%281%29.webp?1715769654',
+        name: 'Shivank Agarwal',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/983/original/microsoft_%281%29_%282%29.webp?1715769840',
+        topic: 'Taught DSA'
+      },
+      {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/985/original/surraj_%282%29.webp?1715770309',
+        name: 'Suraaj Hasija',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/986/original/mastercard_%281%29.webp?1715770428',
+        topic: 'Taught Neural Networks'
+      },
+]
+additional_skills_count: 20
+salary_hike: 45%
+---
+
+# Skills
+- Python
+- Data Processing
+- Data Visualization
+- Analytics
