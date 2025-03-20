06f7eb41edd49f7683d7ae69979609bd4787c86e
agrim-scaler <agrim.nauriyal@scaler.com>
2025-03-20 07:43:16 +0530

    Create AnkurPrakash-Alumni.rb

---
+++ AnkurPrakash-Alumni.rb
@@ -0,0 +1,34 @@
+---
+alumni_name: Ankur Prakash
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/115/124/original/success-story-image_id.png?1742436435'
+role: Senior Data Scientist
+batch: AI & ML September 2022
+location: Bangalore
+card_type: alumni_card
+linkedin_url: 'https://www.linkedin.com/in/ankur-prakash-360540103/'
+whatsapp_number: +91-7209615819
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/115/125/original/images_%281%29.png?1742436462'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/115/126/original/success-story-company_logo_id_%281%29.png?1742436563'
+hours_attended: 809+
+problems_solved: 1,211+
+mentor_details: [
+  {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/982/original/shivank_%281%29.webp?1715769654',
+        name: 'Shivank Agarwal',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/983/original/microsoft_%281%29_%282%29.webp?1715769840',
+        topic: 'Taught Flask'
+      },
+      {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/985/original/surraj_%282%29.webp?1715770309',
+        name: 'Suraaj Hasija',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/986/original/mastercard_%281%29.webp?1715770428',
+        topic: 'Taught ML System Design'
+      },
+  
+]
+additional_skills_count: 24
+---
+
+# Skills
+- Django
+- SQL
