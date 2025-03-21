aeab66ca07b766b1f5420aab11cacefb2e6793a7
agrim-scaler <agrim.nauriyal@scaler.com>
2025-03-21 20:24:19 +0530

    Create AkhileshPratapShahi-Alumni.rb

---
+++ AkhileshPratapShahi-Alumni.rb
@@ -0,0 +1,34 @@
+---
+alumni_name: Akhilesh Pratap Shahi
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/115/939/original/dp.png?1742568794'
+role: Deputy Manager - Data Engineer
+batch: Data Science & ML November 2022	
+location: Bengaluru
+card_type: alumni_card
+linkedin_url: 'https://www.linkedin.com/in/akhileshshahi/'
+whatsapp_number: 	+91-6394187213
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/115/940/original/mphrx_%281%29.png?1742568812'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/066/545/original/success-story-company_logo_id.png?1709041378'
+hours_attended: 194+
+problems_solved: 492+
+mentor_details: [
+    {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/982/original/shivank_%281%29.webp?1715769654',
+        name: 'Shivank Agarwal',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/983/original/microsoft_%281%29_%282%29.webp?1715769840',
+        topic: 'Taught Generative AI'
+      },
+      {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/985/original/surraj_%282%29.webp?1715770309',
+        name: 'Suraaj Hasija',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/986/original/mastercard_%281%29.webp?1715770428',
+        topic: 'Taught Business Case'
+      },
+  
+]
+additional_skills_count: 68
+---
+
+# Skills
+- Python
+- SQL
