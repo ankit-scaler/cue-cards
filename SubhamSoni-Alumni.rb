acead7a2c0910858832e7b539eb08eacbf62c7ab
agrim-scaler <agrim.nauriyal@scaler.com>
2025-03-21 20:40:24 +0530

    Create SubhamSoni-Alumni.rb

---
+++ SubhamSoni-Alumni.rb
@@ -0,0 +1,35 @@
+---
+alumni_name: Subham Soni
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/115/947/original/dp.png?1742569714'
+role: Backend Developer
+batch: Software Development	April 2020
+location: Bengaluru
+card_type: alumni_card
+linkedin_url: 'https://www.linkedin.com/in/subham-soni-samsi/'
+whatsapp_number: +91-9912260642
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/115/948/original/images_%281%29.png?1742569748'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/115/949/original/post_%281%29.png?1742569794'
+hours_attended: 16+
+problems_solved: 115+ 
+mentor_details: [
+  {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/859/original/namanBhalla_%281%29.webp?1715691506',
+    name: 'Naman Bhalla',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585',
+    topic: 'Taught LLD'
+  },
+  {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/861/original/anshuman_%281%29.webp?1715691886',
+    name: 'Anshuman Singh',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/862/original/scaler_%281%29.webp?1715692250',
+    topic: 'Taught HLD'
+  },
+  
+]
+additional_skills_count: 30
+salary_hike: 111%
+---
+
+# Skills
+- Python
+- Spring Boot
