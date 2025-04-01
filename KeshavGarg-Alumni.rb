7f9b7d95296690c420f0bfaabc06a92af92293a2
agrim-scaler <agrim.nauriyal@scaler.com>
2025-04-01 22:34:01 +0530

    Create KeshavGarg-Alumni.rb

---
+++ KeshavGarg-Alumni.rb
@@ -0,0 +1,34 @@
+---
+alumni_name: Keshav Garg
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/117/326/original/dp.png?1743526971'
+role: Data Analyst
+batch: Data Science & ML March 2022
+location: Bengaluru
+card_type: alumni_card
+linkedin_url: 'https://www.linkedin.com/in/itskeshavgarg/'
+whatsapp_number: +91-9878948835
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/064/018/original/success-story-company_logo_id.png?1706879824'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/117/325/original/post_%281%29.png?1743526949'
+hours_attended: 446+
+problems_solved: 431+
+mentor_details: [
+    {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/982/original/shivank_%281%29.webp?1715769654',
+        name: 'Shivank Agarwal',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/983/original/microsoft_%281%29_%282%29.webp?1715769840',
+        topic: 'Taught Advanced DSA'
+      },
+      {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/985/original/surraj_%282%29.webp?1715770309',
+        name: 'Suraaj Hasija',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/986/original/mastercard_%281%29.webp?1715770428',
+        topic: 'Taught Business Case'
+      },
+  
+]
+additional_skills_count: 30
+---
+
+# Skills
+- Gen AI
+- React.js
