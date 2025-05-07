9d8233498c20e33ced4aefcc7c7c757701ca51d5
agrim-scaler <agrim.nauriyal@scaler.com>
2025-05-07 18:15:58 +0530

    Create JagyaseniTripathy-Alumni.rb

---
+++ JagyaseniTripathy-Alumni.rb
@@ -0,0 +1,34 @@
+---
+alumni_name: Jagyaseni Tripathy
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/124/080/original/success-story-image_id.webp?1746621270'
+role: SDE 1
+batch: Software Development	Feburary 2021
+location: Hyderabad
+card_type: alumni_card
+linkedin_url: 'https://www.linkedin.com/in/jagyasenitripathy'
+whatsapp_number: 273+
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/124/081/original/stu.webp?1746621289'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/124/082/original/post_%281%29.webp?1746621307'
+hours_attended: 273+
+problems_solved: 262
+mentor_details: [
+    {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/977/original/pragy_%281%29.webp?1715768351',
+    name: 'Pragy Agarwal',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/978/original/mediaNet.webp?1715768506',
+    topic: 'Taught OS'
+  },
+ {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/096/837/original/abhimanyuw.webp?1731992380',
+    name: 'Abhimanyu Saxena',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/096/838/original/interviewbit_logo_%281%29.webp?1731992393',
+    topic: 'Taught Javascript'
+  },   
+]
+additional_skills_count: 34
+salary_hike: 100%
+---
+
+# Skills
+- Java
+- Spring Boot
