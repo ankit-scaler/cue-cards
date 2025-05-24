a24f17c82a65021321281ad2ec2a1b010c853d1a
ritika-scaler <ritika.bera@scaler.com>
2025-05-24 12:34:12 +0530

    Create AkshatMishra-Alumni.rb
    Created By Ritika

---
+++ AkshatMishra-Alumni.rb
@@ -0,0 +1,36 @@
+---
+alumni_name: Akshat Mishra
+alumni_image_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/128/390/original/Akshat.webp?1748069384
+role: Software Development Engineer III	
+batch: Software Development	March 2021
+location: Bangalore
+card_type: alumni_card
+linkedin_url: https://www.linkedin.com/in/akshat-mishra-05/
+whatsapp_number: 9907601120
+pre_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/128/391/original/narvar_logo.webp?1748069621
+post_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/128/392/original/company_logo.webp?1748069677
+hours_attended: 209
+problems_solved: 187
+mentor_details: [
+  {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/861/original/anshuman_%281%29.webp?1715691886',
+    name: 'Anshuman Singh',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/862/original/scaler_%281%29.webp?1715692250',
+    topic: 'Taught HLD'
+  },
+  {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/977/original/pragy_%281%29.webp?1715768351',
+    name: 'Pragy Agarwal',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/978/original/mediaNet.webp?1715768506',
+    topic: 'Taught LLD'
+  },
+  
+]
+additional_skills_count: 20
+---
+
+# Skills
+- JAVA
+- C++
+- Data Structures
+- MySQL
