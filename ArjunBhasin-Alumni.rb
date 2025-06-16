7d053850978147a9d10c0f436e70fec817050905
ritika-scaler <ritika.bera@scaler.com>
2025-06-16 13:19:59 +0530

    Create ArjunBhasin-Alumni.rb

---
+++ ArjunBhasin-Alumni.rb
@@ -0,0 +1,35 @@
+---
+alumni_name: Arjun Bhasin
+alumni_image_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/132/698/original/Arjun.webp?1750059936
+role: Senior Data Engineer
+batch: Data Science & ML August 2021
+location: Delhi	
+card_type: alumni_card
+linkedin_url: https://www.linkedin.com/in/arjun-bhasin-ds/	
+whatsapp_number: 6377261760
+pre_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/132/699/original/cians_analytics_logo.webp?1750059956
+post_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/132/700/original/insurancedekho_logo.webp?1750060008
+hours_attended: 215.05
+problems_solved: 56
+mentor_details: [
+  {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/859/original/namanBhalla_%281%29.webp?1715691506',
+        name: 'Naman Bhalla',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585',
+        topic: 'Taught LLD'
+  },
+  {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/861/original/anshuman_%281%29.webp?1715691886',
+        name: 'Anshuman Singh',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/862/original/scaler_%281%29.webp?1715692250',
+        topic: 'Taught HLD'
+  },
+  
+]
+additional_skills_count: 45
+---
+
+# Skills
+- Python
+- SQL
+- Data Build Tool (DBT)
