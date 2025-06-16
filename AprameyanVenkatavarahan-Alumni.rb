42e0b08c9f5c750bdbe85163384d7fdb315950ac
ritika-scaler <ritika.bera@scaler.com>
2025-06-16 18:20:42 +0530

    Update AprameyanVenkatavarahan-Alumni.rb

--- AprameyanVenkatavarahan-Alumni.rb
+++ AprameyanVenkatavarahan-Alumni.rb
@@ -2,11 +2,10 @@
 alumni_name: Aprameyan Venkatavarahan
 alumni_image_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/132/679/original/Aprameyan.webp?1750058238
 role: Associate Data Scientist	
-batch: Data Science & ML	Sep 2022	
+batch: Data Science & ML Sep 2022	
 location: Chennai
 card_type: alumni_card
 linkedin_url:https://www.linkedin.com/in/aprameyan-v/	
-whatsapp_number: 
 pre_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/132/680/original/pre_logo.webp?1750058259
 post_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/132/681/original/post_logo.webp?1750058276
 hours_attended: 230.98
@@ -16,7 +15,7 @@ mentor_details: [
         image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/982/original/shivank_%281%29.webp?1715769654',
         name: 'Shivank Agarwal',
         company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/983/original/microsoft_%281%29_%282%29.webp?1715769840',
-        topic: 'Taught Business Case'
+        topic: 'Taught Data science'
   },
   {
         image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/985/original/surraj_%282%29.webp?1715770309',
