4b8b682ff006705460ed3d63aae0a6d84128bdb0
ritika-scaler <ritika.bera@scaler.com>
2025-06-17 12:12:18 +0530

    Update AprameyanVenkatavarahan-Alumni.rb

--- AprameyanVenkatavarahan-Alumni.rb
+++ AprameyanVenkatavarahan-Alumni.rb
@@ -1,14 +1,14 @@
 ---
 alumni_name: Aprameyan Venkatavarahan
-alumni_image_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/132/679/original/Aprameyan.webp?1750058238
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/132/679/original/Aprameyan.webp?1750058238'
 role: Associate Data Scientist	
 batch: Data Science & ML Sep 2022	
 location: Chennai
 card_type: alumni_card
-linkedin_url:https://www.linkedin.com/in/aprameyan-v/	
+linkedin_url: https://www.linkedin.com/in/aprameyan-v/	
 whatsapp_number:
-pre_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/132/680/original/pre_logo.webp?1750058259
-post_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/132/681/original/post_logo.webp?1750058276
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/132/680/original/pre_logo.webp?1750058259'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/132/681/original/post_logo.webp?1750058276'
 hours_attended: 230
 problems_solved: 383
 mentor_details: [
