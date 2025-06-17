9c36e48ab9192b5a588d61cb5a27b7eecbab50a6
ritika-scaler <ritika.bera@scaler.com>
2025-06-17 12:11:29 +0530

    Update ArjunBhasin-Alumni.rb

--- ArjunBhasin-Alumni.rb
+++ ArjunBhasin-Alumni.rb
@@ -1,14 +1,14 @@
 ---
 alumni_name: Arjun Bhasin
-alumni_image_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/132/698/original/Arjun.webp?1750059936
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/132/698/original/Arjun.webp?1750059936'
 role: Senior Data Engineer
 batch: Data Science & ML August 2021
 location: Delhi	
 card_type: alumni_card
 linkedin_url: https://www.linkedin.com/in/arjun-bhasin-ds/	
 whatsapp_number: 6377261760
-pre_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/132/699/original/cians_analytics_logo.webp?1750059956
-post_scaler_org_logo_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/132/700/original/insurancedekho_logo.webp?1750060008
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/132/699/original/cians_analytics_logo.webp?1750059956'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/132/700/original/insurancedekho_logo.webp?1750060008'
 hours_attended: 215
 problems_solved: 56
 mentor_details: [
