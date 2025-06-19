9064c5f96e92abd2b97c86ac6b3f1e233941dd39
ritika-scaler <ritika.bera@scaler.com>
2025-06-19 21:24:38 +0530

    Create AvijitSwain-Alumni.rb

---
+++ AvijitSwain-Alumni.rb
@@ -0,0 +1,35 @@
+---
+alumni_name: Avijit Swain
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/134/066/original/Avijit.webp?1750347837'
+role: Senior data analyst
+batch: Data Science & ML February 2022
+location: Bhubaneswar
+card_type: alumni_card
+linkedin_url: https://www.linkedin.com/in/avijit-swain/
+whatsapp_number: 
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/134/069/original/pre_logo_avijit.webp?1750348095'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/134/070/original/post_scaler_avijit.webp?1750348169'
+hours_attended: 1,163
+problems_solved: 1,494
+mentor_details: [
+  {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/982/original/shivank_%281%29.webp?1715769654',
+        name: 'Shivank Agarwal',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/983/original/microsoft_%281%29_%282%29.webp?1715769840',
+        topic: 'Taught DSA in Python'
+  },
+  {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/985/original/surraj_%282%29.webp?1715770309',
+        name: 'Suraaj Hasija',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/986/original/mastercard_%281%29.webp?1715770428',
+        topic: 'Taught ML System Design'
+  },
+  
+]
+additional_skills_count: 42
+---
+
+# Skills
+- Python
+- SQL
+- Deep Learning
