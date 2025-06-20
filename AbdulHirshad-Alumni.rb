a367c2c6f5095f8ec57a10268b2b282bc149d381
ritika-scaler <ritika.bera@scaler.com>
2025-06-21 00:03:54 +0530

    Create AbdulHirshad-Alumni.rb

---
+++ AbdulHirshad-Alumni.rb
@@ -0,0 +1,35 @@
+---
+alumni_name: Abdul Hirshad
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/134/408/original/abdul.webp?1750442754'
+role: Senior business analyst
+batch: Data Science & ML March 2022
+location: Hyderabad
+card_type: alumni_card
+linkedin_url: https://www.linkedin.com/in/abdul-hirshad-7b789111a/	
+whatsapp_number: 8309152306
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/134/411/original/pre_logo_abdul.webp?1750443056'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/134/412/original/post_org_logo_abdul.webp?1750443171'
+hours_attended: 530
+problems_solved: 932
+mentor_details: [
+  {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/982/original/shivank_%281%29.webp?1715769654',
+        name: 'Shivank Agarwal',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/983/original/microsoft_%281%29_%282%29.webp?1715769840',
+        topic: 'Taught DSA in Python'
+  },
+  {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/985/original/surraj_%282%29.webp?1715770309',
+        name: 'Suraaj Hasija',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/986/original/mastercard_%281%29.webp?1715770428',
+        topic: 'Taught ML System Design'
+  },
+  
+]
+additional_skills_count: 30
+---
+
+# Skills
+- Python
+- NLP
+- Data Analytics
