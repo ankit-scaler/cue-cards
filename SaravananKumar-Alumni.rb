a990bf916f9931e843a95f5e00207d4d449a539d
ritika-scaler <ritika.bera@scaler.com>
2025-06-24 14:28:15 +0530

    Create SaravananKumar-Alumni.rb

---
+++ SaravananKumar-Alumni.rb
@@ -0,0 +1,37 @@
+---
+alumni_name: Saravanan Kumar
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/135/035/original/Saravanan.webp?1750753748'
+role: Software Developer
+batch: Software Development	April 2023
+location: Coimbatore
+card_type: alumni_card
+linkedin_url: https://www.linkedin.com/in/saravanan-k06/
+whatsapp_number: 9080662374
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/135/036/original/Pre_logo_saravanan.webp?1750754126'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/135/037/original/post_logo_saravanan.webp?1750754145'
+hours_attended: 500
+problems_solved: 767
+mentor_details: [
+  {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/859/original/namanBhalla_%281%29.webp?1715691506',
+        name: 'Naman Bhalla',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585',
+        topic: 'Taught LLD'
+  },
+  {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/861/original/anshuman_%281%29.webp?1715691886',
+        name: 'Anshuman Singh',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/862/original/scaler_%281%29.webp?1715692250',
+        topic: 'Taught HLD'
+  },
+  
+]
+additional_skills_count: 98
+---
+
+# Skills
+- DSA
+- Python
+- SQL
+- HLD
+- LLD
