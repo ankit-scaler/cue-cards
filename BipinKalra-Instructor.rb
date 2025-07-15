d7bc5f2a00b9113c8e38fdd573e9ccada539aaae
ritika-scaler <ritika.bera@scaler.com>
2025-07-15 11:12:11 +0530

    Create BipinKalra-Instructor.rb

---
+++ BipinKalra-Instructor.rb
@@ -0,0 +1,45 @@
+---
+instructor_name: Bipin Kalra
+instructor_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/138/698/original/bipin_logo.webp?1752498776'
+class_rating: 4.74
+location: Delhi, India
+card_type: instructor_card
+linkedin_url: 'https://www.linkedin.com/in/bipinkalra/'
+introduction_bio: "Senior Software Engineer - Frontend @Certa.ai.
+Full Stack Engineer & UI/UX Specialist.
+React.js • Python • Web Performance.
+Educator & Freelancer.
+Ex-Scaler, Spinny, Coding Blocks"
+num_learners: 7062
+hour_classes: 1035
+work_history: [
+    {
+        "company": "Certa.ai",
+        "title": 'Senior Software Engineer - Frontend',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/138/701/original/bipin_company1_logo.webp?1752500217"
+    },
+    {
+        "company": "Scaler",
+        "title": 'SDE 2 and Module Owner',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/138/702/original/bipin_company2_logo.webp?1752500248"
+    },
+    {
+        "company": "Spinny",
+        "title": 'Software Developer',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/138/703/original/bipin_company3_logo.webp?1752500274"
+    },
+]
+students_working_at_company_logos: [
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/745/original/google.webp?1715593708",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/746/original/amazon.webp?1715593782",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/747/original/microsoft.webp?1715593910",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/748/original/swiggy.webp?1715594132",
+
+]
+---
+
+# Skill Tags
+- Python
+- Neural Networks
+- Data Science
+- Amazon Web Services (AWS)
