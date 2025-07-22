5a0fbc5a53627cb8d00d8dbd772a3691b5508964
ritika-scaler <ritika.bera@scaler.com>
2025-07-22 14:53:00 +0530

    Create ShishirSharma-Alumni.rb

---
+++ ShishirSharma-Alumni.rb
@@ -0,0 +1,35 @@
+---
+alumni_name: Shishir Sharma
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/140/212/original/Shishir.webp?1753175398'
+role: Jr Data Analyst	
+batch: Data Science & ML January 2024
+location: Bangalore
+card_type: alumni_card
+linkedin_url: 'https://www.linkedin.com/in/shishir-sharma-07064810a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app'
+whatsapp_number: 9724768142
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/140/213/original/Shishir_pre_scaler.webp?1753175752'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/140/214/original/Shishir_post_scaler.webp?1753175773'
+hours_attended: 335
+problems_solved: 300
+mentor_details: [
+  {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/982/original/shivank_%281%29.webp?1715769654',
+        name: 'Shivank Agarwal',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/983/original/microsoft_%281%29_%282%29.webp?1715769840',
+        topic: 'Taught Python'
+      },
+      {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/985/original/surraj_%282%29.webp?1715770309',
+        name: 'Suraaj Hasija',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/986/original/mastercard_%281%29.webp?1715770428',
+        topic: 'Taught Business Case'
+      },
+  
+]
+additional_skills_count: 50
+---
+
+# Skills
+- Python
+- SQL
+- Data Analytics
