14993f833db9cd95e646cc3def7873bc75be37fb
ritika-scaler <ritika.bera@scaler.com>
2025-07-25 19:07:43 +0530

    Create LalitSingh-Alumni.rb

---
+++ LalitSingh-Alumni.rb
@@ -0,0 +1,35 @@
+---
+alumni_name: Lalit Singh
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/140/892/original/Lalit.webp?1753449441'
+role: System Engineer
+batch: DSML September 2023
+location: Gurgaon
+card_type: alumni_card
+linkedin_url: 'https://www.linkedin.com/in/lalit-singh-68aa03139/'
+whatsapp_number: 8755396310
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/140/894/original/lalit_pre.webp?1753450371'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/140/895/original/lalit_post.webp?1753450447'
+hours_attended: 646
+problems_solved: 1059
+mentor_details: [
+  {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/982/original/shivank_%281%29.webp?1715769654',
+        name: 'Shivank Agarwal',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/983/original/microsoft_%281%29_%282%29.webp?1715769840',
+        topic: 'Taught Advance DSA'
+  },
+  {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/985/original/surraj_%282%29.webp?1715770309',
+        name: 'Suraaj Hasija',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/986/original/mastercard_%281%29.webp?1715770428',
+        topic: 'Taught Business Case'
+  },
+]
+additional_skills_count: 63
+---
+
+# Skills
+- Python
+- AWS
+- Data Science
+- Docker
