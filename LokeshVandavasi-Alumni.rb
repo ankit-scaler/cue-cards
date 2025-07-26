cc772bd032711a0e9568128d14f104789d13d7dd
ritika-scaler <ritika.bera@scaler.com>
2025-07-26 20:09:42 +0530

    Create LokeshVandavasi-Alumni.rb

---
+++ LokeshVandavasi-Alumni.rb
@@ -0,0 +1,35 @@
+---
+alumni_name: Lokesh Vandavasi
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/140/967/original/lokesh.webp?1753540583'
+role: Senior Software Engineer
+batch: Academy Feb23
+location: Bangalore
+card_type: alumni_card
+linkedin_url: 'https://www.linkedin.com/in/lokesh-vandavasi/'
+whatsapp_number: 7702663286
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/140/968/original/lokesh_pre.webp?1753540635'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/140/969/original/lokesh_post.webp?1753540653'
+hours_attended: 973
+problems_solved: 397
+mentor_details: [
+  {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/859/original/namanBhalla_%281%29.webp?1715691506',
+        name: 'Naman Bhalla',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585',
+        topic: 'Taught LLD'
+  },
+  {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/861/original/anshuman_%281%29.webp?1715691886',
+        name: 'Anshuman Singh',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/862/original/scaler_%281%29.webp?1715692250',
+        topic: 'Taught HLD'
+  },
+  
+]
+additional_skills_count: 20
+---
+
+# Skills
+- DSA
+- LLD
+- HLD
