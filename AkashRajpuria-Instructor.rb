f30448e813c9a4542c902de902c3409057570d78
ritika-scaler <ritika.bera@scaler.com>
2025-07-29 18:05:02 +0530

    Create AkashRajpuria-Instructor.rb

---
+++ AkashRajpuria-Instructor.rb
@@ -0,0 +1,43 @@
+---
+instructor_name: Akash Rajpuria
+instructor_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/141/275/original/Akash.webp?1753791286'
+class_rating: 4.68
+location: Mumbai, Maharashtra, India
+card_type: instructor_card
+linkedin_url: 'https://www.linkedin.com/in/akashrajpuria/'
+introduction_bio: "Senior Data Scientist at EPAM.  
+BITS PILANI.
+Ex-RELIANCE ,PFIZER.
+"
+num_learners: 7434
+hour_classes: 1592
+work_history: [
+    {
+        "company": "EPAM Systems",
+        "title": 'Senior Data Scientist',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/141/276/original/Akash_epam.webp?1753792058"
+    },
+    {
+        "company": "MResult",
+        "title": 'Lead Data Scientist - NLP- PFIZER',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/141/280/original/Akash_MResult.webp?1753792270"
+    },
+    {
+        "company": "Jio Platforms Limited (JPL)",
+        "title": 'Machine Learning Specialist',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/141/282/original/Akash_jio.webp?1753792393"
+    },
+]
+students_working_at_company_logos: [
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/745/original/google.webp?1715593708",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/746/original/amazon.webp?1715593782",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/747/original/microsoft.webp?1715593910",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/748/original/swiggy.webp?1715594132",
+
+]
+---
+
+# Skill Tags
+- LLMOps
+- ETL
+- GANs
