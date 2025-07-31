f6b9a9e46c15f286cc8f63cd28aebc9d969bac24
ritika-scaler <ritika.bera@scaler.com>
2025-07-31 14:59:44 +0530

    Create VijayanandR-Alumni.rb

---
+++ VijayanandR-Alumni.rb
@@ -0,0 +1,35 @@
+---
+alumni_name: Vijayanand R
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/141/682/original/Vijayanand.webp?1753952811'
+role: Software Engineer
+batch: Software Development	December 2022
+location: Chennai
+card_type: alumni_card
+linkedin_url: 'https://www.linkedin.com/in/vijayanand-r-456973198/'
+whatsapp_number: 8754833291
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/141/683/original/Vijayanand_pre.webp?1753953300'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/141/684/original/Vijayanand_post.webp?1753953324'
+hours_attended: 547
+problems_solved: 350
+mentor_details: [
+  {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/859/original/namanBhalla_%281%29.webp?1715691506',
+    name: 'Naman Bhalla',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585',
+    topic: 'Taught LLD'
+  },
+  {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/861/original/anshuman_%281%29.webp?1715691886',
+    name: 'Anshuman Singh',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/862/original/scaler_%281%29.webp?1715692250',
+    topic: 'Taught HLD'
+  },  
+]
+additional_skills_count: 26
+---
+
+# Skills
+- Python
+- SQL
+- AWS
+- Django
