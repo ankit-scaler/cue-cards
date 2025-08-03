41823d8c46830f0daca32a5381b3967e47c9e15f
ritika-scaler <ritika.bera@scaler.com>
2025-08-03 23:23:18 +0530

    Create VikramKavide-Alumni.rb

---
+++ VikramKavide-Alumni.rb
@@ -0,0 +1,35 @@
+---
+alumni_name: Vikram Kavide
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/142/055/original/Vikram.webp?1754243132'
+role: Senior Frontend Developer
+batch: Software Development	July 2023
+location: Hyderabad
+card_type: alumni_card
+linkedin_url: 'https://www.linkedin.com/in/vikram-kavide-ab2507213'
+whatsapp_number: 8790985880
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/142/057/original/Vikram_pre.webp?1754243410'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/142/058/original/Vikram_post.webp?1754243432'
+hours_attended: 185
+problems_solved: 128
+mentor_details: [
+  {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/859/original/namanBhalla_%281%29.webp?1715691506',
+        name: 'Naman Bhalla',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585',
+        topic: 'Taught LLD'
+  },
+  {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/861/original/anshuman_%281%29.webp?1715691886',
+        name: 'Anshuman Singh',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/862/original/scaler_%281%29.webp?1715692250',
+        topic: 'Taught HLD'
+  },
+]
+additional_skills_count: 29
+---
+
+# Skills
+- Python
+- System Design
+- TypeScript
+- DSA
