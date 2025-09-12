198252628b30e9cbda1ae2ade1c8aa65e555ce6a
ritika-scaler <ritika.bera@scaler.com>
2025-09-12 19:19:27 +0530

    Create ShubhamSoni-Alumni.rb

---
+++ ShubhamSoni-Alumni.rb
@@ -0,0 +1,37 @@
+---
+alumni_name: Shubham Soni
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/150/691/original/Shubham_Soni.webp?1757684239'
+role: Backend Developer	
+batch: Software Development	April 2020
+location: Burhanpur
+card_type: alumni_card
+linkedin_url: 'https://www.linkedin.com/in/subham-soni-771760b8/'
+whatsapp_number: 9912260642
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/150/693/original/subham_soni_pre_scaler.webp?1757684439'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/150/694/original/subham_soni_post_scaler.webp?1757684450'
+hours_attended: 16
+problems_solved: 300
+mentor_details: [
+  {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/859/original/namanBhalla_%281%29.webp?1715691506',
+        name: 'Naman Bhalla',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585',
+        topic: 'Taught LLD'
+  },
+  {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/861/original/anshuman_%281%29.webp?1715691886',
+        name: 'Anshuman Singh',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/862/original/scaler_%281%29.webp?1715692250',
+        topic: 'Taught HLD'
+  },
+  
+]
+additional_skills_count: 40
+salary_hike: 
+---
+
+# Skills
+- Software Design
+- Docker
+- HLD
+- LLD
