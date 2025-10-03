ec845cbf0bea4cb16162a3201465c215684c36fe
ritika-scaler <ritika.bera@scaler.com>
2025-10-03 22:40:10 +0530

    Create AyushRaj-Instructor.rb

---
+++ AyushRaj-Instructor.rb
@@ -0,0 +1,39 @@
+---
+instructor_name: Ayush Raj
+instructor_image_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/156/829/original/Ayush_instructor.webp?1759510842
+class_rating: 4.85
+location: Delhi, India
+card_type: instructor_card
+linkedin_url: 'https://www.linkedin.com/in/ayush-raj-642a4215/'
+introduction_bio: "Senior Manager
+Full Stack Developer
+Lead Instructor
+Building AI Tools & Agentic Systems
+Hugging Face Certified"
+num_learners: 968
+hour_classes: 635
+work_history: [
+    {
+        "company": "Scaler",
+        "title": 'Senior Manager, Software Enginnering & Lead Instructor',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/156/831/original/ayush_company_logo.webp?1759511183"
+    },
+    {
+        "company": "GlobalLogic",
+        "title": 'Consultant and Tech Lead',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/156/832/original/globallogic_logo.webp?1759511285"
+    },
+]
+students_working_at_company_logos: [
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/745/original/google.webp?1715593708",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/746/original/amazon.webp?1715593782",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/747/original/microsoft.webp?1715593910",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/748/original/swiggy.webp?1715594132",
+
+]
+---
+
+# Skill Tags
+- MERN Stack
+- Oops
+- ETL
