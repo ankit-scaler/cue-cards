53b6e33a2ccfeae350e7d3c2ede99895823f791e
ritika-scaler <ritika.bera@scaler.com>
2025-10-11 10:29:45 +0530

    Create VivekPandey-Alumni.rb

---
+++ VivekPandey-Alumni.rb
@@ -0,0 +1,37 @@
+---
+alumni_name: Vivek Pandey
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/158/798/original/vivek.webp?1760158341'
+role: ML Analyst
+batch: Data Science & ML Jul-22
+location: Faridabad, Haryana, India
+card_type: alumni_card
+linkedin_url: 'https://www.linkedin.com/in/vivek-pandey-6b220b194/'
+whatsapp_number: 9910269984
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/158/801/original/vivek_pre.webp?1760158759'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/158/799/original/vivek_post.webp?1760158359'
+hours_attended: 610
+problems_solved: 1100
+mentor_details: [
+  {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/982/original/shivank_%281%29.webp?1715769654',
+        name: 'Shivank Agarwal',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/983/original/microsoft_%281%29_%282%29.webp?1715769840',
+        topic: 'Taught Advance DSA'
+  },
+  {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/985/original/surraj_%282%29.webp?1715770309',
+        name: 'Suraaj Hasija',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/986/original/mastercard_%281%29.webp?1715770428',
+        topic: 'Taught Business Case'
+  },
+  
+]
+additional_skills_count: 10
+salary_hike: 
+---
+
+# Skills
+- NLP
+- Machine Learning
+- Computer Vision
+- Image Processing
