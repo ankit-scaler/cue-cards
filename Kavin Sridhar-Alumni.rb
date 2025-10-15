0aaa688c1f717e80ac3a8e795738bdc15a5a78f6
ritika-scaler <ritika.bera@scaler.com>
2025-10-15 18:41:41 +0530

    Create Kavin Sridhar-Alumni.rb

---
+++ Kavin Sridhar-Alumni.rb
@@ -0,0 +1,37 @@
+---
+alumni_name: Kavin Sridhar
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/159/695/original/kavin.webp?1760524987'
+role: Software Engineer 2
+batch: Software Development	June 2023
+location: Tiruchirappalli, Tamil Nadu, India
+card_type: alumni_card
+linkedin_url: 'https://www.linkedin.com/in/kavinraju/'
+whatsapp_number: 9688625269
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/159/738/original/kavin_pre.webp?1760533632'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/159/739/original/kavin_post.webp?1760533662'
+hours_attended: 660
+problems_solved: 1100
+mentor_details: [
+  {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/859/original/namanBhalla_%281%29.webp?1715691506',
+    name: 'Naman Bhalla',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585',
+    topic: 'Taught LLD'
+  },
+  {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/861/original/anshuman_%281%29.webp?1715691886',
+    name: 'Anshuman Singh',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/862/original/scaler_%281%29.webp?1715692250',
+    topic: 'Taught HLD'
+  },
+  
+]
+additional_skills_count: 60
+salary_hike: 
+---
+
+# Skills
+- HLD
+- LLD
+- DSA
+- MySQL
