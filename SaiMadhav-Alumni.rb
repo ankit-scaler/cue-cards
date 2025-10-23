71f7047b22e8892b7bf416b55c2417b5fb98f0c9
ritika-scaler <ritika.bera@scaler.com>
2025-10-23 17:42:22 +0530

    Create SaiMadhav-Alumni.rb

---
+++ SaiMadhav-Alumni.rb
@@ -0,0 +1,37 @@
+---
+alumni_name: Sai Madhav
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/160/601/original/sai.webp?1761218010'
+role: Senior Data Scientist
+batch: Data Science & ML July 2023
+location: Kakinada, India
+card_type: alumni_card
+linkedin_url: 'https://www.linkedin.com/in/sai-madhav-allam/'
+whatsapp_number: 9848209783
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/160/609/original/sai_pre.webp?1761221372'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/160/610/original/sai_post.webp?1761221380'
+hours_attended: 369
+problems_solved: 59
+mentor_details: [
+  {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/982/original/shivank_%281%29.webp?1715769654',
+        name: 'Shivank Agarwal',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/983/original/microsoft_%281%29_%282%29.webp?1715769840',
+        topic: 'Taught Advance DSA'
+  },
+  {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/985/original/surraj_%282%29.webp?1715770309',
+        name: 'Suraaj Hasija',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/986/original/mastercard_%281%29.webp?1715770428',
+        topic: 'Taught Business Case'
+  },
+  
+]
+additional_skills_count: 50
+salary_hike: 
+---
+
+# Skills
+- Computer Vision
+- NLP
+- Docker
+- MLOps
