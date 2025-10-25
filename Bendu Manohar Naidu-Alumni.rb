7553ce4391416c971352f537ccc9ddbea4a3be50
ritika-scaler <ritika.bera@scaler.com>
2025-10-25 10:44:17 +0530

    Create Bendu Manohar Naidu-Alumni.rb

---
+++ Bendu Manohar Naidu-Alumni.rb
@@ -0,0 +1,37 @@
+---
+alumni_name: Bendu Manohar Naidu
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/160/988/original/bendu2.webp?1761368912'
+role: Associate Software Development Engineer	
+batch: Software Development	July 2022
+location: Hyderabad, India
+card_type: alumni_card
+linkedin_url: 'https://www.linkedin.com/in/manoharnaidubendu/'
+whatsapp_number: 9949798571
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/160/990/original/bendu_pre.webp?1761369239'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/160/989/original/bendu_post.webp?1761369135'
+hours_attended: 595
+problems_solved: 300
+mentor_details: [
+  {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/859/original/namanBhalla_%281%29.webp?1715691506',
+        name: 'Naman Bhalla',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585',
+        topic: 'Taught LLD'
+  },
+  {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/861/original/anshuman_%281%29.webp?1715691886',
+        name: 'Anshuman Singh',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/862/original/scaler_%281%29.webp?1715692250',
+        topic: 'Taught HLD'
+  },
+  
+]
+additional_skills_count: 40
+salary_hike: 
+---
+
+# Skills
+- HLD
+- LLD
+- AWS
+- Kafka
