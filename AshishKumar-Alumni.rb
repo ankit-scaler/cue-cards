f565bed6f117e19fabc3ab2045c528dfb581a8da
ritika-scaler <ritika.bera@scaler.com>
2025-10-28 16:12:16 +0530

    Create AshishKumar-Alumni.rb

---
+++ AshishKumar-Alumni.rb
@@ -0,0 +1,37 @@
+---
+alumni_name: Ashish Kumar
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/161/561/original/AshishK.webp?1761648056'
+role: Backend Developer
+batch: Software Development	October 2019
+location: Pune, India
+card_type: alumni_card
+linkedin_url: 'http://linkedin.com/in/ashishkumar8087'
+whatsapp_number: 8087340097
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/161/562/original/AshishK_pre.webp?1761648063'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/161/563/original/AshishK_post.webp?1761648090'
+hours_attended: 8
+problems_solved: 248
+mentor_details: [
+  {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/859/original/namanBhalla_%281%29.webp?1715691506',
+    name: 'Naman Bhalla',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585',
+    topic: 'Taught LLD'
+  },
+  {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/861/original/anshuman_%281%29.webp?1715691886',
+    name: 'Anshuman Singh',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/862/original/scaler_%281%29.webp?1715692250',
+    topic: 'Taught HLD'
+  },
+  
+]
+additional_skills_count: 44
+salary_hike: 
+---
+
+# Skills
+- REST APIs
+- Java
+- Kotlin
+- GCP
