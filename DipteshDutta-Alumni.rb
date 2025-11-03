aefb5fcc86ad24b282177927986fdebe83a0eb48
ritika-scaler <ritika.bera@scaler.com>
2025-11-03 14:10:50 +0530

    Create DipteshDutta-Alumni.rb

---
+++ DipteshDutta-Alumni.rb
@@ -0,0 +1,36 @@
+---
+alumni_name: Diptesh Dutta
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/162/913/original/Diptesh.webp?1762159117'
+role: Member of Technical Staff
+batch: Software Development	May 2023
+location: Kolkata, India
+card_type: alumni_card
+linkedin_url: 'http://linkedin.com/in/dipteshdutta'
+whatsapp_number: 8910170109
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/162/914/original/Diptesh_pre.webp?1762159122'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/162/915/original/Diptesh_post.webp?1762159133'
+hours_attended: 482
+problems_solved: 447
+mentor_details: [
+  {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/859/original/namanBhalla_%281%29.webp?1715691506',
+    name: 'Naman Bhalla',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585',
+    topic: 'Taught LLD'
+  },
+  {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/861/original/anshuman_%281%29.webp?1715691886',
+    name: 'Anshuman Singh',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/862/original/scaler_%281%29.webp?1715692250',
+    topic: 'Taught HLD'
+  },  
+]
+additional_skills_count: 30
+salary_hike: 
+---
+
+# Skills
+- OOPs
+- Java
+- LLD
+- Python
