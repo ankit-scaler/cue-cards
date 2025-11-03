a000099d662c67513507c5d9aedac2fba762dab2
ritika-scaler <ritika.bera@scaler.com>
2025-11-03 14:01:38 +0530

    Create Swayam Dash-Alumni.rb

---
+++ Swayam Dash-Alumni.rb
@@ -0,0 +1,36 @@
+---
+alumni_name: Swayam Dash
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/162/902/original/Swayam.webp?1762158410'
+role: MLOps Engineer
+batch: Data Science & ML February 2022
+location: Bangalore, India
+card_type: alumni_card
+linkedin_url: 'https://www.linkedin.com/in/swayam-dash/'
+whatsapp_number: 8637283288
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/162/903/original/Swayam_pre.webp?1762158414'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/162/904/original/Swayam_post.webp?1762158435'
+hours_attended: 748
+problems_solved: 1339
+mentor_details: [
+  {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/982/original/shivank_%281%29.webp?1715769654',
+    name: 'Shivank Agarwal',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/983/original/microsoft_%281%29_%282%29.webp?1715769840',
+    topic: 'Taught Advance Python'
+  },
+  {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/985/original/surraj_%282%29.webp?1715770309',
+    name: 'Suraaj Hasija',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/986/original/mastercard_%281%29.webp?1715770428',
+    topic: 'Taught MLOps'
+  },  
+]
+additional_skills_count: 26
+salary_hike: 
+---
+
+# Skills
+- MLOps
+- PostgreSQL
+- Machine Learning
+- Keras
