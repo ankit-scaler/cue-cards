3c312561c181a1530cf4a8cd655b1cddcfe070e0
ritika-scaler <ritika.bera@scaler.com>
2025-11-06 16:10:16 +0530

    Create PrateekAgrawal-Alumni.rb

---
+++ PrateekAgrawal-Alumni.rb
@@ -0,0 +1,37 @@
+---
+alumni_name: Prateek Agrawal
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/163/990/original/Prateek.webp?1762425570'
+role: Software Engineer (Backend )
+batch: Software Development	December 2021
+location: Pune, India
+card_type: alumni_card
+linkedin_url: 'https://www.linkedin.com/in/prateek-agrawal58/'
+whatsapp_number: 6260605982
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/163/991/original/Prateek_pre.webp?1762425580'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/163/992/original/Prateek_post.webp?1762425594'
+hours_attended: 146
+problems_solved: 500
+mentor_details: [
+  {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/859/original/namanBhalla_%281%29.webp?1715691506',
+        name: 'Naman Bhalla',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585',
+        topic: 'Taught LLD'
+  },
+  {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/861/original/anshuman_%281%29.webp?1715691886',
+        name: 'Anshuman Singh',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/862/original/scaler_%281%29.webp?1715692250',
+        topic: 'Taught HLD'
+  },
+  
+]
+additional_skills_count: 53
+salary_hike: 
+---
+
+# Skills
+- PostgreSQL
+- Django
+- NLP
+- MongoDB
