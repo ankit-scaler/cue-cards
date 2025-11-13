66d16a7a6ccd2cfe17792ebb7d6809f28bc6e76e
ritika-scaler <ritika.bera@scaler.com>
2025-11-13 13:30:24 +0530

    Create ArpitSharma-Alumni.rb

---
+++ ArpitSharma-Alumni.rb
@@ -0,0 +1,37 @@
+---
+alumni_name: Arpit Sharma
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/164/965/original/arpit.webp?1763020700'
+role: SDE II
+batch: Software Development	October 2022
+location: Delhi, India
+card_type: alumni_card
+linkedin_url: 'https://www.linkedin.com/in/arpit3043/'
+whatsapp_number: 8445726929
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/164/966/original/arpit_pre.webp?1763020706'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/164/967/original/arpit_post.webp?1763020720'
+hours_attended: 372
+problems_solved: 312
+mentor_details: [
+  {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/859/original/namanBhalla_%281%29.webp?1715691506',
+        name: 'Naman Bhalla',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585',
+        topic: 'Taught LLD'
+  },
+  {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/861/original/anshuman_%281%29.webp?1715691886',
+        name: 'Anshuman Singh',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/862/original/scaler_%281%29.webp?1715692250',
+        topic: 'Taught HLD'
+  }
+  
+]
+additional_skills_count: 40
+salary_hike: 
+---
+
+# Skills
+- Java
+- REST APIs
+- GCP
+- LLD
