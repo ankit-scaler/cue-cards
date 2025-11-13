9978d000219dc687bacff1e1423e263833196775
ritika-scaler <ritika.bera@scaler.com>
2025-11-13 13:40:35 +0530

    Create RohitDas-Alumni.rb

---
+++ RohitDas-Alumni.rb
@@ -0,0 +1,36 @@
+---
+alumni_name: Rohit Das
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/164/970/original/rohit.webp?1763021191'
+role: Software Development Engineer 2
+batch: Software Development	2021
+location: Bangalore, India
+card_type: alumni_card
+linkedin_url: 'https://www.linkedin.com/in/rohitdas2000'
+whatsapp_number: 9163621033
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/164/971/original/rohit_pre.webp?1763021199'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/164/972/original/rohit_post.webp?1763021215'
+hours_attended: 666
+problems_solved: 300
+mentor_details: [
+  {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/861/original/anshuman_%281%29.webp?1715691886',
+    name: 'Anshuman Singh',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/862/original/scaler_%281%29.webp?1715692250',
+    topic: 'Taught HLD'
+  },
+  {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/977/original/pragy_%281%29.webp?1715768351',
+    name: 'Pragy Agarwal',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/978/original/mediaNet.webp?1715768506',
+    topic: 'Taught LLD'
+  }  
+]
+additional_skills_count: 40
+salary_hike: 
+---
+
+# Skills
+- Java
+- Oops
+- Spring Boot
+- Rest API
