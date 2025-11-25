e29bed618396f11f401ce618e9337fc45203ba43
ritika-scaler <ritika.bera@scaler.com>
2025-11-25 16:17:41 +0530

    Create OmPrajapat-Alumni.rb

---
+++ OmPrajapat-Alumni.rb
@@ -0,0 +1,36 @@
+---
+alumni_name: Om Prajapat
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/167/512/original/OM.webp?1764067483'
+role: Solutions Architect
+batch: Data Science & ML	2022
+location: Noida, India
+card_type: alumni_card
+linkedin_url: 'https://www.linkedin.com/in/om-prakash-prajapat/'
+whatsapp_number: 7827927988
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/167/513/original/OM_pre.webp?1764067488'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/167/514/original/OM_post.webp?1764067502'
+hours_attended: 600
+problems_solved: 500
+mentor_details: [
+  {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/982/original/shivank_%281%29.webp?1715769654',
+    name: 'Shivank Agarwal',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/983/original/microsoft_%281%29_%282%29.webp?1715769840',
+    topic: 'Taught Advance DSA'
+  },
+  {
+    image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/985/original/surraj_%282%29.webp?1715770309',
+    name: 'Suraaj Hasija',
+    company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/986/original/mastercard_%281%29.webp?1715770428',
+    topic: 'Taught Case Study'
+  },  
+]
+additional_skills_count: 30
+salary_hike: 
+---
+
+# Skills
+- AWS
+- Reinforcement Learning
+- Deep Learning
+- Docker
