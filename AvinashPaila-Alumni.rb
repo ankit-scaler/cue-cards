6386f4aea5a0501c5739c5ea12f0cbffc706478c
ritika-scaler <ritika.bera@scaler.com>
2025-12-04 14:31:47 +0530

    Create AvinashPaila-Alumni.rb

---
+++ AvinashPaila-Alumni.rb
@@ -0,0 +1,36 @@
+---
+alumni_name: Avinash Paila
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/169/066/original/Avinash.webp?1764838612'
+role: Software Engineer 3
+batch: Software Development August 2021
+location: Kharagpur, India
+card_type: alumni_card
+linkedin_url: 'https://www.linkedin.com/in/avinash-paila-43314b123/'
+whatsapp_number: 7602770851
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/169/068/original/Avinash_pre.webp?1764838740'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/169/069/original/Avinash_post.webp?1764838746'
+hours_attended: 222
+problems_solved: 500
+mentor_details: [
+   {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/859/original/namanBhalla_%281%29.webp?1715691506',
+        name: 'Naman Bhalla',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585',
+        topic: 'Taught LLD'
+    },
+    {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/861/original/anshuman_%281%29.webp?1715691886',
+        name: 'Anshuman Singh',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/862/original/scaler_%281%29.webp?1715692250',
+        topic: 'Taught HLD'
+    },  
+]
+additional_skills_count: 30
+salary_hike: 
+---
+
+# Skills
+- React
+- Ruby
+- Redux
+- GraphQL
