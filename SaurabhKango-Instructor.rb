f01709f8e3c2976ae699785edc262e06e610ba27
ritika-scaler <ritika.bera@scaler.com>
2025-12-18 18:27:15 +0530

    Create SaurabhKango-Instructor.rb

---
+++ SaurabhKango-Instructor.rb
@@ -0,0 +1,39 @@
+---
+instructor_name: Saurabh Kango
+instructor_image_url: https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/171/161/original/SaurabhKango_instructor-removebg-preview.webp?1766062188
+class_rating: 4.75
+location: Bengaluru, Karnataka, India
+card_type: instructor_card
+linkedin_url: 'https://www.linkedin.com/in/saurabhkango/'
+introduction_bio: "DS, Analytics & Insights @Meesho. 
+Ex -LinkedIn, Airbnb, Mu Sigma.
+MBA(IIFT).
+COEP"
+num_learners: 1800
+hour_classes: 1824
+work_history: [
+    {
+        "company": "LinkedIn",
+        "title": 'Insights Manager',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/171/163/original/SaurabhKango_1.webp?1766062469"
+      },
+      {
+        "company": "Airbnb Experiences",
+        "title": "Analytics and Insights Analyst",
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/171/164/original/SaurabhKango_2.webp?1766062479"
+      },
+]
+students_working_at_company_logos: [
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/745/original/google.webp?1715593708",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/746/original/amazon.webp?1715593782",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/747/original/microsoft.webp?1715593910",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/748/original/swiggy.webp?1715594132",
+
+]
+---
+
+# Skill Tags
+- Applied Machine Learning
+- Python
+- Data Analytics
+- ETL
