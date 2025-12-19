5d47b4dbf90532c26debbd91e28f72eb38f247bf
ritika-scaler <ritika.bera@scaler.com>
2025-12-19 16:03:58 +0530

    Create SaiDattaSamayamanthula-Alumni.rb

---
+++ SaiDattaSamayamanthula-Alumni.rb
@@ -0,0 +1,36 @@
+---
+alumni_name: Sai Datta Samayamanthula
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/171/293/original/saidatta.webp?1766140231'
+role: Software Development Engineer 3
+batch: Software Development 2025
+location: Bengaluru, Karnataka, India
+card_type: alumni_card
+linkedin_url: 'https://www.linkedin.com/in/snsndatta/'
+whatsapp_number: 8870731119
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/171/294/original/saidatta_pre.webp?1766140238'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/171/295/original/saidatta_post.webp?1766140253'
+hours_attended: 140
+problems_solved: 353
+mentor_details: [
+  {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/859/original/namanBhalla_%281%29.webp?1715691506',
+        name: 'Naman Bhalla',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/860/original/google_%281%29.webp?1715691585',
+        topic: 'Taught LLD'
+  },
+  {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/861/original/anshuman_%281%29.webp?1715691886',
+        name: 'Anshuman Singh',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/862/original/scaler_%281%29.webp?1715692250',
+        topic: 'Taught HLD'
+  },
+]
+additional_skills_count: 60
+salary_hike: 
+---
+
+# Skills
+- HLD
+- LLD
+- JAVA
+- Spring Boot
