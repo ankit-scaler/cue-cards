006df2f2a30e7359f3663abaa317aeeaf3f9466b
ritika-scaler <ritika.bera@scaler.com>
2025-12-29 14:08:38 +0530

    Create ErankiSrikanth-Alumni.rb

---
+++ ErankiSrikanth-Alumni.rb
@@ -0,0 +1,37 @@
+---
+alumni_name: Eranki Srikanth
+alumni_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/172/036/original/Eranki.webp?1766996096'
+role: Product Manager 
+batch: Data Science & ML 2023
+location: Hyderabad, Telangana, India
+card_type: alumni_card
+linkedin_url: 'https://www.linkedin.com/in/srikanth-eranki-87657b110/'
+whatsapp_number: 9441264438
+pre_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/172/037/original/Eranki_pre.webp?1766996119'
+post_scaler_org_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/172/038/original/Eranki_post.webp?1766996225'
+hours_attended: 463
+problems_solved: 600
+mentor_details: [
+  {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/982/original/shivank_%281%29.webp?1715769654',
+        name: 'Shivank Agarwal',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/983/original/microsoft_%281%29_%282%29.webp?1715769840',
+        topic: 'Taught Advance DSA'
+  },
+  {
+        image: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/985/origtinal/surraj_%282%29.webp?1715770309',
+        name: 'Suraaj Hasija',
+        company_logo_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/986/original/mastercard_%281%29.webp?1715770428',
+        topic: 'Taught Business Case'
+  },
+  
+]
+additional_skills_count: 75
+salary_hike: 
+---
+
+# Skills
+- Project Management
+- CRM
+- Sprint Planning
+- Financial Analysis
