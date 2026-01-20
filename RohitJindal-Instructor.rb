bd6e672043652708e445dc08e0eb519ff07e0af0
ritika-scaler <ritika.bera@scaler.com>
2026-01-20 11:47:50 +0530

    Create RohitJindal-Instructor.rb

---
+++ RohitJindal-Instructor.rb
@@ -0,0 +1,39 @@
+---
+instructor_name: Rohit Jindal
+instructor_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/175/848/original/Rohit_Jindal.webp?1768889504'
+class_rating: 4.71
+location: Bengaluru, KA, India
+card_type: instructor_card
+linkedin_url: 'https://www.linkedin.com/in/jindal-rohit540/'
+introduction_bio: "Senior Data Scientist @ Target.
+IISc Bangalore (AIR 79).
+University of Cambridge.
+DTU(DCE) Rank Opener."
+num_learners: 1900
+hour_classes: 947
+work_history: [
+    {
+        "company": "Target",
+        "title": 'Senior Data Scientist',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/175/850/original/Rohit_target.webp?1768889696"
+    },
+    {
+        "company": "Mastercard AI Garage",
+        "title": "Data Scientist",
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/175/851/original/Rohit_mastercard.webp?1768889740"
+    },
+]
+students_working_at_company_logos: [
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/745/original/google.webp?1715593708",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/746/original/amazon.webp?1715593782",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/747/original/microsoft.webp?1715593910",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/748/original/swiggy.webp?1715594132",
+
+]
+---
+
+# Skill Tags
+- Artificial Intelligence (AI)
+- Neural Networks
+- LLMOps
+- Python Expert
