97414ed4eea02d076f9326a59d907dccf9d7a250
ritika-scaler <ritika.bera@scaler.com>
2026-02-04 16:20:43 +0530

    Create ShashwatBagaria-Instructor.rb

---
+++ ShashwatBagaria-Instructor.rb
@@ -0,0 +1,37 @@
+---
+instructor_name: Shashwat Bagaria
+instructor_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/178/600/original/Shashwat.webp?1770202135'
+class_rating: 4.78
+location: Lucknow, Uttar Pradesh, India
+card_type: instructor_card
+linkedin_url: 'https://www.linkedin.com/in/shashwat-bagaria/'
+introduction_bio: "Ex-Senior SDE 1 - Assistant Manager at JioHotstar.
+Ex-Senior SDE 1 - Assistant Manager at Viacom18 Media Private Limited.
+"
+num_learners: 3250
+hour_classes: 618
+work_history: [
+    {
+        "company": "JioHotstar",
+        "title": 'Senior SDE 1 - Assistant Manager',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/178/597/original/Shashwat1.webp?1770202100"
+    },
+    {
+        "company": "Viacom18 Media Private Limited",
+        "title": 'Senior SDE 1 - Assistant Manager',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/178/598/original/Shashwat2.webp?1770202109"
+    },
+]
+students_working_at_company_logos: [
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/745/original/google.webp?1715593708",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/746/original/amazon.webp?1715593782",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/747/original/microsoft.webp?1715593910",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/748/original/swiggy.webp?1715594132",
+
+]
+---
+
+# Skill Tags
+- ReactJS
+- NodeJS
+- Systems Design
