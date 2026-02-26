5626525f84eb0f36e22f2f685f4e65a3badc5424
ritika-scaler <ritika.bera@scaler.com>
2026-02-26 15:23:36 +0530

    Create SabeelKhan-Instructor.rb

---
+++ SabeelKhan-Instructor.rb
@@ -0,0 +1,37 @@
+---
+instructor_name: Sabeel Khan
+instructor_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/181/955/original/Sabeel-removebg-preview.webp?1772099324'
+class_rating: 4.93
+location: Delhi, India
+card_type: instructor_card
+linkedin_url: 'https://www.linkedin.com/in/sabeelhps/'
+introduction_bio: "Engineering @ PayU.
+Lazypay."
+num_learners: 159
+hour_classes: 218
+work_history: [
+    {
+        "company": "PayU",
+        "title": 'Software Engineer',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/181/956/original/sabeel_payu_logo.webp?1772099417"
+      },
+      {
+        "company": "Scaler",
+        "title": "Remote Instructor, Mentor",
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/181/957/original/sabeel_scaler.webp?1772099435"
+      },
+]
+students_working_at_company_logos: [
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/745/original/google.webp?1715593708",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/746/original/amazon.webp?1715593782",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/747/original/microsoft.webp?1715593910",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/748/original/swiggy.webp?1715594132",
+
+]
+---
+
+# Skill Tags
+- MERN Stack
+- MongoDB
+- Spring Boot
+- Java
