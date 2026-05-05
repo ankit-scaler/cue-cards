2e0f4b474b2894da83956c8d6cc7a975ff1fff1d
ritika-scaler <ritika.bera@scaler.com>
2026-05-05 17:27:08 +0530

    Create ShubhamSoni-Instructor.rb

---
+++ ShubhamSoni-Instructor.rb
@@ -0,0 +1,43 @@
+---
+instructor_name: Shubham Soni
+instructor_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/194/452/original/Shubham_Soni-removebg-preview.webp?1777982193'
+class_rating: 4.8
+location: Bengaluru, Karnataka, India
+card_type: instructor_card
+linkedin_url: 'https://www.linkedin.com/in/subham-soni-771760b8/'
+introduction_bio: "SWE @Google.
+ex Intuit.
+BITS Pilani
+"
+num_learners: 5432
+hour_classes: 1111
+work_history: [
+    {
+        "company": "Google",
+        "title": 'Software Engineer',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/194/449/original/google_logo.webp?1777981640"
+    },
+    {
+        "company": "Intuit",
+        "title": 'SDE-2',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/194/450/original/intuit_logo.webp?1777981899"
+    },
+    {
+        "company": "Credit Suisse",
+        "title": 'SDE 2',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/194/451/original/credit_suisse_logo.webp?1777981961"
+    },
+]
+students_working_at_company_logos: [
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/745/original/google.webp?1715593708",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/746/original/amazon.webp?1715593782",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/747/original/microsoft.webp?1715593910",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/748/original/swiggy.webp?1715594132",
+
+]
+---
+
+# Skill Tags
+- LLMOps
+- ETL
+- GANs
