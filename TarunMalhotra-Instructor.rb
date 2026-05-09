60cc32b801190efcfc75488ac84faa6917232955
ritika-scaler <ritika.bera@scaler.com>
2026-05-09 14:03:06 +0530

    Create TarunMalhotra-Instructor.rb

---
+++ TarunMalhotra-Instructor.rb
@@ -0,0 +1,42 @@
+---
+instructor_name: Tarun Malhotra
+instructor_image_url: 'https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/195/520/original/Tarun-removebg-preview.png?1778315375'
+class_rating: 4.81
+location: Delhi, India
+card_type: instructor_card
+linkedin_url: 'https://www.linkedin.com/in/tarun-malhotra-72973aa6/'
+introduction_bio: "Lead Software Engineer. 
+Ex Google, DirectI, Scaler Academy"
+num_learners: 1324
+hour_classes: 962
+work_history: [
+    {
+        "company": "Microsoft",
+        "title": 'Senior Software Engineer',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/195/517/original/Tarun1.webp?1778315310"
+    },
+    {
+        "company": "Google",
+        "title": 'Software Engineer',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/195/518/original/Tarun2.webp?1778315318"
+    },
+    {
+        "company": "Ethos",
+        "title": 'Software Engineer',
+        "company_logo_url": "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/195/519/original/Tarun3.webp?1778315340"
+    },
+]
+students_working_at_company_logos: [
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/745/original/google.webp?1715593708",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/746/original/amazon.webp?1715593782",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/747/original/microsoft.webp?1715593910",
+    "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/073/748/original/swiggy.webp?1715594132",
+
+]
+---
+
+# Skill Tags
+- Python
+- Java
+- SQL
+- Computer Vision
